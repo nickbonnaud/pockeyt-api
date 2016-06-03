8e5d19e91baa1c413bf6f931f9b9f58e9151a6b8
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-06-03 08:49:19 -0400

    trying to force migration

--- database/migrations/2016_05_26_190131_add_pictures_to_posts_table.php
+++ database/migrations/2016_05_26_190131_add_pictures_to_posts_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPicturesToPostsTable extends Migration
+class AddPictureToPostsTable extends Migration
 {
     /**
      * Run the migrations.
