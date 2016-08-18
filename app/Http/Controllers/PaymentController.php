fe0a87cf96e5085685f009ab545c4f5532389a24
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-08-18 17:44:37 -0400

    added payment controller & route

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -11,7 +10,7 @@ class PaymentController extends Controller
 {
     
     public function clientToken() {
-        $clientToken = Braintree_ClientToken::generate();
+        $clientToken = Braintree\ClientToken::generate();
         return $clientToken;
     }
 
