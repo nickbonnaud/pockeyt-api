7a8836dff58d2537631232452cb9863a081fd37e
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-08-24 12:57:31 -0400

    added jwt and updated db with fbID

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -99,6 +99,6 @@ class AuthenticateController extends Controller
         $payload = JWTFactory::make($credentials);
         $token = JWTAuth::encode($payload);
         
-        return (['token' => $token]);
+        return Response::json(['token' => $token->get()]);
     }
 }
