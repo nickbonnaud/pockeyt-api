1e37a7900e40b50a6f6b7808d299b6c83c8231cb
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-08-24 11:30:10 -0400

    added jwt and updated db with fbID

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -44,7 +44,7 @@ class AuthenticateController extends Controller
 
 
         $credentials = $request->only('email', 'password');
-
+        return $credentials;
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
