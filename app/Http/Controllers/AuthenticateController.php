bd83b08dfbf9ca6458615fb242ae2dbf5014844d
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-08-24 09:02:03 -0400

    added jwt and updated db with fbID

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -62,7 +62,7 @@ class AuthenticateController extends Controller
         $client = new \GuzzleHttp\Client();
 
         try {
-            $fbResponse = $client->request('GET', 'https://graph.facebook.com/me', [
+            $response = $client->request('GET', 'https://graph.facebook.com/me', [
                 'query' => ['fields' => 'name, email, picture', 'access_token' => $token, ]
             ]);
         } catch (RequestException $e) {
@@ -70,7 +70,10 @@ class AuthenticateController extends Controller
                 return $e->getResponse();
             }
         }
-        $response = $fbResponse->json();
+
+        $data = $response->getBody();
+
+        return gettype($data);
 
         $newUser = $response->only('name', 'email');
         $newUser['fbID'] = $response->id;
