e097c5c16e7c9c182dbf1c5f888c286bc21e642b
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-08-24 10:05:06 -0400

    added jwt and updated db with fbID

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -73,7 +73,7 @@ class AuthenticateController extends Controller
 
         $data = (string) $response->getBody();
 
-        return json_decode($data);
+        return $data->name;
 
         $newUser['fbID'] = $data->id;
         if ($data->picture->data->is_silhouette === false) {
