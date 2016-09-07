5ca73c6fb77ff364c0bd3df77118ee301e57a67d
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-09-07 11:46:45 -0400

    returning payment methods

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -33,7 +33,7 @@ class UsersController extends Controller
         }
 
         $customer = \Braintree_Customer::find($user->customer_id);
-        return $customer;
+        return $customer->last4;
 
     
 
