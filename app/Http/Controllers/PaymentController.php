fce1c8d4e0f49820897079dff6f61458966c4250
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-09-06 20:06:53 -0400

    returning payment methods

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -29,7 +29,7 @@ class PaymentController extends Controller
             $dbUser = User::findOrFail($authUser->id);
             $dbUser['customer_id'] = $result->customer->id;
             $dbUser->save();
-            return $result->customer->paymentMethods;
+            return $result->customer->paymentMethods[0];
         } else {
             foreach($result->errors->deepAll() AS $error) {
                 return($error->code . ": " . $error->message . "\n");
