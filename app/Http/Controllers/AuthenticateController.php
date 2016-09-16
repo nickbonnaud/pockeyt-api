26b1dd2b81a38e6af6010a20100b59f2b44a022a
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-09-16 07:51:27 -0400

    added user return to logins

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -32,7 +32,7 @@ class AuthenticateController extends Controller
         } else {
             // grab credentials from the request
             $credentials = $request->only('email', 'password');
-            return $credentials->email;
+            $user = User::where('email', '=', $request->input('email'))->first();
 
             try {
                 // attempt to verify the credentials and create a token for the user
@@ -44,8 +44,8 @@ class AuthenticateController extends Controller
                 return response()->json(['error' => 'could_not_create_token'], 500);
             }
 
-            // all good so return the token
-            return response()->json(compact('token'));
+            $user['token'] = $token;
+            return response()->json(compact('user'));
         }
     }
 
