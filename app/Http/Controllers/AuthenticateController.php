5916927535ec6e8306ac118e5f4ffe7b6b0d5296
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-09-16 07:42:41 -0400

    added user return to logins

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -32,7 +32,7 @@ class AuthenticateController extends Controller
         } else {
             // grab credentials from the request
             $credentials = $request->only('email', 'password');
-            return response($credentials -> email);
+            return $credentials;
 
             try {
                 // attempt to verify the credentials and create a token for the user
