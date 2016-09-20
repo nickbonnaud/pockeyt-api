699c216c775b2da1e3c62f3c36eec3c07df09a82
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-09-20 10:43:13 -0400

    returning distance from stored lat lngs

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Profile;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -14,9 +15,42 @@ class GeoController extends Controller
     {
     	$user = User::findOrFail($request->userId);
     	$user['lat'] = $request->lat;
-    	$user['long'] = $request->long;
+    	$user['lng'] = $request->long;
     	$user['accuracy'] = $request->accuracy;
     	$user['timestamp'] = $request->timestamp;
-    	return $user;
+    	checkDistance($user);
     }
+
+    private function checkDistance($user) {
+    	$businesses = DB('profiles')->select(array('id', 'lat', 'lng'))->get();
+    	$userLat = $user->lat;
+    	$userLng = $user->lng;
+    	foreach ($businesses as $business) {
+    		$businessLat = $business->lat;
+    		$businessLng = $business->lng;
+
+    		if (($businessLat !== NULL) && ($businessLng !== NULL)) {
+    			$distance = getDistanceFromLatLng($businessLat, $businessLng, $userLat, $userLng);
+    			return $distance;
+    		}
+    	}
+    }
+
+    private function getDistanceFromLatLng($businessLat, $businessLng, $userLat, $userLng) {
+    	$r = 6371000; // Radius of the earth in m
+	    $dLat = deg2rad($userLat-$businessLat);  // deg2rad below
+	    $dLon = deg2rad($userLng-$businessLng); 
+	    $a = 
+	      Math.sin($dLat/2) * Math.sin($dLat/2) +
+	      Math.cos(deg2rad($businessLat)) * Math.cos(deg2rad($userLat)) * 
+	      Math.sin($dLon/2) * Math.sin($dLon/2)
+	      ; 
+	    $c = 2 * Math.atan2(Math.sqrt($a), Math.sqrt(1-$a)); 
+	    $d = R * $c; // Distance in m
+	    return $d;
+    }
+
+    private function deg2rad($eg) {
+    	return $deg * (Math.PI/180);
+   }
 }
