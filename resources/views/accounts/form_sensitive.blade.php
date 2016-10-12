bbe1e93ad48a0f538a923332f061eeebebf0a8c2
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-10-12 13:34:49 -0400

    removed test check from braintree

--- resources/views/accounts/form_sensitive.blade.php
+++ resources/views/accounts/form_sensitive.blade.php
@@ -2,13 +2,13 @@
   <div class="form-group">
     <label for="accountNumber4" class="col-sm-2 control-label">Full Account Number</label>
     <div class="col-sm-10">
-      <input type="text" name="accountNumber4" class="form-control" id="accountNumber4" value="{{ $account->accountNumber4 }}" required>
+      <input type="text" name="accountNumber4" class="form-control" id="accountNumber4" value="*** **** {{ $account->accountNumber4 }}" required>
     </div>
   </div>
   <div class="form-group">
    <label for="routingNumber4" class="col-sm-2 control-label">Full Routing Number</label>
     <div class="col-sm-10">
-      <input type="integer" name="routingNumber4" class="form-control" id="routingNumber4" value="{{ $account->routingNumber4 }}" required>
+      <input type="integer" name="routingNumber4" class="form-control" id="routingNumber4" value="*** ** {{ $account->routingNumber4 }}" required>
     </div>
   </div>
   <div class="modal-footer">
