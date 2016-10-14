08809302dcc5b45845ff1ab5329f8fd536e8f998
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-10-14 14:05:53 -0400

    using vue.js to display list of customers in location

--- app/Events/CustomerEnterRadius.php
+++ app/Events/CustomerEnterRadius.php
@@ -11,7 +11,7 @@ class CustomerEnterRadius extends Event implements ShouldBroadcast
     use SerializesModels;
 
     public $user;
-    private $business;
+    public $business;
 
     /**
      * Create a new event instance.
