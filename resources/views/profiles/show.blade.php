928997a6518aadc693ffabc59d773e97f44a5801
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-10-14 15:49:58 -0400

    using vue.js to display list of customers in location

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -17,13 +17,10 @@
             <!-- Content Header (Page header) -->
             <section class="content-header">
               <h1>
-                Blank page
-                <small>it all starts here</small>
+                Customer Dashboard
               </h1>
               <ol class="breadcrumb">
                 <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                <li><a href="#">Examples</a></li>
-                <li class="active">Blank page</li>
               </ol>
             </section>
 
@@ -31,27 +28,19 @@
             <section class="content">
 
               <!-- Default box -->
-              <div class="box">
-                <div class="box-header with-border">
-                  <h3 class="box-title">Title</h3>
+              <div id="customer">
+                <template v-for="user in users">
+                  <div class="col-md-6">
+                    <div class="box box-primary">
+                      <div class="box-header with-border">
+                        <h3 class="box-title">@{{user.first_name}} @{{user.last_name}}</h3>
+                        <div class="box-body">
 
-                  <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                      <i class="fa fa-minus"></i></button>
-                    <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-                      <i class="fa fa-times"></i></button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div class="box-body">
-                  <ul id="customer">
-                    <li v-for="user in users">@{{ user.first_name }}</li>
-                  </ul>
-                </div>
-                <!-- /.box-body -->
-                <div class="box-footer">
-                  Footer
-                </div>
-                <!-- /.box-footer-->
+                </template>
               </div>
               <!-- /.box -->
 
