b5be695539bc3813f1a0e94cfee9e25cbd583bfb
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-02 15:37:56 -0400

    adding connect capability

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -53,7 +53,7 @@ class ConnectController extends Controller
 		$client = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
 
 		try {
-			$response = $client->request('GET', '$pageID/subscribed_apps', [
+			$response = $client->request('GET', $pageID, [
         'query' => ['access_token' => $access_token ]
       ]);
 		} catch (RequestException $e) {
