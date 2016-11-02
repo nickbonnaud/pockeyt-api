cced7e0a34e0d386f3e3fa3c988a3ce38cb1b28d
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-02 03:36:35 -0400

    adding connect capability

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -28,7 +28,7 @@ class ConnectController extends Controller
 	private function isLoggedInFB($hasCode) {
 		if (! $hasCode) return $this->getAuthorization();
 		$userManagedPages = Socialite::driver('facebook')->fields(['accounts'])->user();
-		dd($userManagedPages->user->accounts);
+		dd($userManagedPages->user);
 	}
 
 	private function getAuthorization() {
