671cee39a0971bc69414899368168a5bdf59ebaf
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-08 12:46:41 -0500

    add events

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -47,7 +47,7 @@ class ConnectController extends Controller
 		$client = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
 
 		try {
-			$response = $client->request('POST', $pageID . '/events', [
+			$response = $client->request('GET', $pageID . '/events', [
         'query' => ['access_token' => $access_token ]
       ]);
 		} catch (RequestException $e) {
