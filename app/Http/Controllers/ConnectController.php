a4b583233ef3bcf5dd7863de842fbd61e54dc163
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-09 18:30:07 -0500

    adding auto events

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -44,22 +44,78 @@ class ConnectController extends Controller
 
 	private function installApp($pageID, $access_token) {
 		
-		$client = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
+		$businesses = Profile::whereNotNull('fb_page_id')->whereNotNull('fb_app_id')->get();
 
-		try {
-			$response = $client->request('POST', $pageID . '/subscribed_apps', [
-        'query' => ['access_token' => $access_token ]
-      ]);
-		} catch (RequestException $e) {
-			if ($e->hasResponse()) {
-				dd($e->getResponse());
-        return $e->getResponse();
-      }
-		}
-		$data = json_decode($response->getBody());
-		if ($data->success === true) {
-			return $this->addPageIdToProfile($pageID, $access_token);
-		}
+        foreach ($businesses as $business) {
+
+            $pageID = $business->fb_page_id;
+            $access_token = $business->fb_app_id;
+
+            $client = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
+
+            try {
+                $currentTime = time();
+                $response = $client->request('GET', $pageID . '/events?since=' . $currentTime, [
+                    'query' => ['access_token' => $access_token ]
+                ]);
+            } catch (RequestException $e) {
+                if ($e->hasResponse()) {
+                    dd($e->getResponse());
+                    return $e->getResponse();
+                }
+            }
+            $data = json_decode($response->getBody());
+            $events = $data->data;
+
+            foreach ($events as $event) {
+                $existingEvent = Post::where('fb_post_id', '=', $event->id);
+                if ($existingEvent === null) {
+                    $post = new Post;
+                    $post->title = $event->name;
+                    $post->body = $event->description;
+                    $post->fb_post_id = $event->id;
+                    $post->published_at = Carbon::now(new DateTimeZone(config('app.timezone')));
+                    
+                    $date = strtotime($event->start_time);
+                    $formattedDate = date('Y-m-d', $time);
+                    $post->event_date = $formattedDate;
+
+                    $clientPhoto = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
+                    try {
+                        $responsePhoto = $clientPhoto->request('GET', $event->id . '/picture', [
+                            'query' => ['redirect' => '0', 'access_token' => $access_token ]
+                        ]);
+                    } catch (RequestException $e) {
+                        if ($e->hasResponse()) {
+                            dd($e->getResponse());
+                            return $e->getResponse();
+                        }
+                    }
+                    $dataPhoto = json_decode($responsePhoto->getBody());
+                    $post->photo_path = $dataPhoto->data->url;
+
+                    $business->posts()->save($post);
+                }
+            }
+        }
+
+
+		// $client = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
+
+		// try {
+		// 	$response = $client->request('POST', $pageID . '/subscribed_apps', [
+  //       'query' => ['access_token' => $access_token ]
+  //     ]);
+		// } catch (RequestException $e) {
+		// 	if ($e->hasResponse()) {
+		// 		dd($e->getResponse());
+  //       return $e->getResponse();
+  //     }
+		// }
+		// $data = json_decode($response->getBody());
+		// if ($data->success === true) {
+		// 	return $this->addPageIdToProfile($pageID, $access_token);
+		// }
 	}
 
 	private function addPageIdToProfile($pageID, $access_token) {
