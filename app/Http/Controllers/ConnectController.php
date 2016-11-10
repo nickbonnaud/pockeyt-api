088d7465edcdb6e362a75f4eefbabd75e1b6c926
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-10 09:20:17 -0500

    adding insta sub

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -132,8 +132,7 @@ class ConnectController extends Controller
 	private function getInstaPost($accountId, $mediaId) {
 			$profile = Profile::where('insta_account_id', '=', $accountId)->first();
 			$access_token = $profile->insta_account_token;
-			$clientInsta = new \GuzzleHttp\Client(['base_uri' => 'https://api.instagram.com/v1/media']);
-			event(new BusinessFeedUpdate($clientInsta));
+			$clientInsta = new \GuzzleHttp\Client(['base_uri' => 'https://api.instagram.com/v1/media/']);
 			try {
 				$responseInsta = $clientInsta->request('GET', $mediaId, [
 	        'query' => ['access_token' => $access_token ]
