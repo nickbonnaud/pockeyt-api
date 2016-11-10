da7073e5dddf805cf61be9e2d48e163b9d6b8a67
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-10 02:35:10 -0500

    adding insta sub

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -121,7 +121,7 @@ class ConnectController extends Controller
 	public function receiveInstaMedia(Request $request) {
 		$body = $request->getContent();
 		$data = json_decode($body, true);
-		$stuff = $data[0]->object_id;
+		$stuff = $data[0];
 		event(new BusinessFeedUpdate($stuff));
 	}
 
