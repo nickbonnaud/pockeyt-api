8e4602bfe5809479c9e8248286da75fea60dc866
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-09 21:22:07 -0500

    adding auto events

--- app/Console/Commands/AddEvents.php
+++ app/Console/Commands/AddEvents.php
@@ -42,62 +42,59 @@ class AddEvents extends Command
      */
     public function handle()
     {
-        
-        event(new BusinessFeedUpdate("hello"));
+        $businesses = Profile::whereNotNull('fb_page_id')->whereNotNull('fb_app_id')->get();
 
-        // $businesses = Profile::whereNotNull('fb_page_id')->whereNotNull('fb_app_id')->get();
+        foreach ($businesses as $business) {
 
-        // foreach ($businesses as $business) {
+            $pageID = $business->fb_page_id;
+            $access_token = $business->fb_app_id;
 
-        //     $pageID = $business->fb_page_id;
-        //     $access_token = $business->fb_app_id;
+            $client = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
 
-        //     $client = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
-
-        //     try {
-        //         $currentTime = time();
-        //         $response = $client->request('GET', $pageID . '/events', [
-        //             'query' => ['since' => $currentTime,'access_token' => $access_token ]
-        //         ]);
-        //     } catch (RequestException $e) {
-        //         if ($e->hasResponse()) {
-        //             dd($e->getResponse());
-        //             return $e->getResponse();
-        //         }
-        //     }
-        //     $data = json_decode($response->getBody());
-        //     $events = $data->data;
-        //     foreach ($events as $event) {
-        //         event(new BusinessFeedUpdate($event));
-        //         $existingEvent = Post::where('fb_post_id', '=', $event->id)->first();
-        //         if ($existingEvent === null) {
-        //             $post = new Post;
-        //             $post->title = $event->name;
-        //             $post->body = $event->description;
-        //             $post->fb_post_id = $event->id;
-        //             $post->published_at = Carbon::now(new DateTimeZone(config('app.timezone')));
+            try {
+                $currentTime = time();
+                $response = $client->request('GET', $pageID . '/events', [
+                    'query' => ['since' => $currentTime,'access_token' => $access_token ]
+                ]);
+            } catch (RequestException $e) {
+                if ($e->hasResponse()) {
+                    dd($e->getResponse());
+                    return $e->getResponse();
+                }
+            }
+            $data = json_decode($response->getBody());
+            $events = $data->data;
+            foreach ($events as $event) {
+                event(new BusinessFeedUpdate($event));
+                $existingEvent = Post::where('fb_post_id', '=', $event->id)->first();
+                if ($existingEvent === null) {
+                    $post = new Post;
+                    $post->title = $event->name;
+                    $post->body = $event->description;
+                    $post->fb_post_id = $event->id;
+                    $post->published_at = Carbon::now(new DateTimeZone(config('app.timezone')));
                     
-        //             $date = strtotime($event->start_time);
-        //             $formattedDate = date('Y-m-d', $date);
-        //             $post->event_date = $formattedDate;
+                    $date = strtotime($event->start_time);
+                    $formattedDate = date('Y-m-d', $date);
+                    $post->event_date = $formattedDate;
 
-        //             $clientPhoto = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
-        //             try {
-        //                 $responsePhoto = $clientPhoto->request('GET', $event->id . '/picture', [
-        //                     'query' => ['redirect' => '0', 'access_token' => $access_token ]
-        //                 ]);
-        //             } catch (RequestException $e) {
-        //                 if ($e->hasResponse()) {
-        //                     dd($e->getResponse());
-        //                     return $e->getResponse();
-        //                 }
-        //             }
-        //             $dataPhoto = json_decode($responsePhoto->getBody());
-        //             $post->photo_path = $dataPhoto->data->url;
+                    $clientPhoto = new \GuzzleHttp\Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
+                    try {
+                        $responsePhoto = $clientPhoto->request('GET', $event->id . '/picture', [
+                            'query' => ['redirect' => '0', 'access_token' => $access_token ]
+                        ]);
+                    } catch (RequestException $e) {
+                        if ($e->hasResponse()) {
+                            dd($e->getResponse());
+                            return $e->getResponse();
+                        }
+                    }
+                    $dataPhoto = json_decode($responsePhoto->getBody());
+                    $post->photo_path = $dataPhoto->data->url;
 
-        //             $business->posts()->save($post);
-        //         }
-        //     }
-        // }
+                    $business->posts()->save($post);
+                }
+            }
+        }
     }
 }
