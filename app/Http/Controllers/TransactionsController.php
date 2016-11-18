f6c31e72b5a1f4e7006e15d2399773059679c9d1
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-18 14:16:36 -0500

    update location in db

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Location;
 use App\Http\Requests;
 
 use App\Http\Controllers\Controller;
@@ -11,7 +12,8 @@ class TransactionsController extends Controller
 {
     
     public function createTransaction($customerId) {
-        dd($customerId);
+        $locationCheck = Location::where('user_id', '=', $customerId)->first();
+        dd($locationCheck);
     }
 
 }
