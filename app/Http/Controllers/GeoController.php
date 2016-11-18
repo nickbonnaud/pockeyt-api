ecabee9ea541aa856637286cdf71dadf34cf4936
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-18 13:57:06 -0500

    update location in db

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -42,7 +42,7 @@ class GeoController extends Controller
     			if ($distance <= 1000) {
                     $inLocations[] = $business->id;
                     $prevLocations = $user->prevLocations;
-                    $locationCheck = Location::where('user_id', '=', $user->id);
+                    $locationCheck = Location::where('user_id', '=', $user->id)->first();
                     event(new CustomerEnterRadius($user, $business, $locationCheck));
                     if (!isset($prevLocations)) {
                         $dbUser->locations()->create([
