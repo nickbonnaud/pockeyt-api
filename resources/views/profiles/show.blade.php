0d13646a90a3033cbb443565ccf4b80c4ced55d1
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-18 00:02:58 -0500

    added user tile in home

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -41,7 +41,7 @@
                 </a>
               </div>
               <div class="box-footer">
-                <a :href="{{ url('transactions/create') }}/user.id" class="btn btn-primary btn-block">
+                <a v-on:click="goToTransaction(user.id)" class="btn btn-primary btn-block">
                 <b>Create Charge</b>
               </a>
               </div>
@@ -144,6 +144,10 @@
               }
             }
           }
+        }
+        goToTransaction: function(customerId) {
+          route = "{{ route('transactions/create', ['customerId' => '?id?']) }}"
+          location.href = route.replace('?id?', customerId)
         }
       })
     </script>
