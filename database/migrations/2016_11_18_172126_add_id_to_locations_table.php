57e8b0f84fba1f05a268860910f09da689426032
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-18 17:24:31 -0500

    update location in db

---
+++ database/migrations/2016_11_18_172126_add_id_to_locations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIdToLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->increments('id')->first();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->dropColumn('id');
+        });
+    }
+}
