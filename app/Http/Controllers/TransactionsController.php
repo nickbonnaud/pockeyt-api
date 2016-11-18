b5babcea9c408600bd578e77165f9a9cddf2bf10
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-18 14:55:36 -0500

    update location in db

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -15,8 +15,9 @@ class TransactionsController extends Controller
     public function createTransaction($customerId) {
         $user = User::findOrFail($customerId);
         $business = 113;
-        $locationCheck = Location::where(function ($query, $user, $business) {
-            $query->where('user_id', '=', $user->id)
+        $userId = $user->id;
+        $locationCheck = Location::where(function ($query, $userId, $business) {
+            $query->where('user_id', '=', $userId)
                 ->where('location_id', '=', $business);
         })->first();
         dd($locationCheck);
