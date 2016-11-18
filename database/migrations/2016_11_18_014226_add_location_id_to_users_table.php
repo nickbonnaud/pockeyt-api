4b46c72c10636771fac735706d023e97aba70177
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-18 01:48:05 -0500

    added location_id to users table

---
+++ database/migrations/2016_11_18_014226_add_location_id_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLocationIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('location_id')->nullable()->after('photo_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('location_id');
+        });
+    }
+}
