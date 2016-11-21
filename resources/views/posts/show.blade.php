626b8c0d614fc98a31aff4ce050ee0630dee2fd4
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-21 18:25:38 -0500

    added OG supported links

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
 <head>
     <meta property="og:type" content="article" />
     <meta property="og:url" content="https:www.pockeyt-test.com/posts/{{ $post->id }}" /> 
-    <!-- <meta property="og:title" content="Pockeyt post by {{ $profile->business_name }}" /> -->
+    <meta property="og:title" content="Pockeyt post by {{ $profile->business_name }}" />
     @if(! is_null($post->photo_path))
         <meta property="og:image"  content="{{ $post->photo_path }}" />
     @endif
@@ -13,34 +13,26 @@
     <div class="row">
         <div class="col-md-12">
 
-            @if(is_null($post))
-
-                <h2 class="text-center">Sorry! Looks like this post was deleted! :(</h2>
-
-            @else
-
-               <!--  <img class="photoLogo" src="{{ $profile->logo->url }}">
-                <p class="partnername">{{ $profile->business_name }}</p> -->
-                @if(! is_null($post->title))
-                    <p class="postTitle">{{ $post->title }}</p>
-                @endif
-
-                <p><img class="postPhoto" src="{{ $post->photo_path }}"></p>
-                <hr>
-                @if(! is_null($post->formatted_body))
-                    <article class="postText">
-                        {!!  $post->formatted_body !!}
-                    </article>
-                @else
-                    <article class="postText">
-                        Hello
-                    </article>
-                @endif
-                <hr>
-                <div class="footer-date">{{ $post->published_at->diffForHumans() }}</div>
-                <p class="signature">- Brought to you by <a href="http://www.pockeyt.com/">Pockeyt</a>
-
+            <img class="photoLogo" src="{{ $profile->logo->url }}">
+            <p class="partnername">{{ $profile->business_name }}</p>
+            @if(! is_null($post->title))
+                <p class="postTitle">{{ $post->title }}</p>
             @endif
+
+            <p><img class="postPhoto" src="{{ $post->photo_path }}"></p>
+            <hr>
+            @if(! is_null($post->formatted_body))
+                <article class="postText">
+                    {!!  $post->formatted_body !!}
+                </article>
+            @else
+                <article class="postText">
+                    Hello
+                </article>
+            @endif
+            <hr>
+            <div class="footer-date">{{ $post->published_at->diffForHumans() }}</div>
+            <p class="signature">- Brought to you by <a href="http://www.pockeyt.com/">Pockeyt</a>
 
         </div>
     </div>
