0d9c629497e67ae1c51eb9d7787e2255991287ba
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-23 14:54:32 -0500

    rendering inventory in vuejs

--- resources/views/transactions/bill_show.blade.php
+++ resources/views/transactions/bill_show.blade.php
@@ -5,7 +5,7 @@
 <div class="content-wrapper">
   <section class="content-header">
     <h1>
-      Customer Dashboard
+      {{$customer->first_name}} {{$customer->last_name}}'s bill
     </h1>
     <ol class="breadcrumb">
       <li><a href="{{ route('profiles.show', ['profiles' => $user->profile->id])  }}"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -24,30 +24,10 @@
     <div class="scroll-contents">
       @include('partials.transactions.inventory', ['inventory' => $inventory])  
     </div>
-    
   </div>
 </section>
 
-<!-- <template id="products-template">
-  <div>
-  <div class="col-md-3" v-for="product in inventory">
-    <div class="box">
-      <div class="box-header with-border">
-        <h3 class="box-title">Pizza</h3>
-      </div>
-      <div class="box-body">
-        Hello
-      </div>
-    </div>
-  </div>
-  </div>
-</template> -->
-
 @stop
-<!-- @section('scripts.footer')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.0.1/vue.js"></script>
-<script src="/js/vuejs.js"></script>
-@stop -->
 
 
 
