4577c5812ae75a250544bc1b45454328ee160860
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-23 15:54:19 -0500

    added thumbnail path to products table

--- resources/views/partials/transactions/inventory.blade.php
+++ resources/views/partials/transactions/inventory.blade.php
@@ -4,7 +4,7 @@
       <div class="box">
         <div class="box-body">
           @if(! is_null($product->product_photo_path))
-            <img src="{{ $product->product_photo_path }}" class="product-image">
+            <img src="{{ $product->product_tn_photo_path }}" class="product-image">
           @else
             <img src="{{ asset('/images/noImage.png') }}" class="product-image">
           @endif
