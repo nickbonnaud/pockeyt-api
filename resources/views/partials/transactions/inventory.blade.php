eb9482bea59bb0f4a031d8cbffb213fcf17160ba
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-23 16:38:47 -0500

    added thumbnail path to products table

--- resources/views/partials/transactions/inventory.blade.php
+++ resources/views/partials/transactions/inventory.blade.php
@@ -1,8 +1,8 @@
 @if(count($inventory) > 0)
   @foreach($inventory as $product)
   	<div class="col-md-3">
-      <div class="box">
-        <div class="box-body">
+      <div class="box-inventory">
+        <div class="box-body-inventory">
           @if(! is_null($product->product_photo_path))
             <img src="{{ $product->product_tn_photo_path }}">
           @else
