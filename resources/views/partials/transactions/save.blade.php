382e8041aa2e0f5f1631abcaf323c45f775ce02e
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-29 18:45:34 -0500

    adding persistence to bill

--- resources/views/partials/transactions/save.blade.php
+++ resources/views/partials/transactions/save.blade.php
@@ -8,7 +8,7 @@
     <input type="submit" value="Keep Open" class="btn btn-block btn-primary btn-xs">
   </form>
 @else
-	<form action="{{ route('bill.store) }}" method="post">
+	<form action="{{ route('bill.store') }}" method="post">
     <input type="hidden" name="user_id" value="{{ $customer->id }}">
     <input type="hidden" name="paid" value="false">
     <input type="hidden" name="products" value="@{{ JSON.stringify(this.bill) }}">
