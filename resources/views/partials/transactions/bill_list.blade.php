495d478c4f4d1540d7f628af57da137f1cdcd5d2
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-28 22:19:51 -0500

    updated how price is handled

--- resources/views/partials/transactions/bill_list.blade.php
+++ resources/views/partials/transactions/bill_list.blade.php
@@ -3,6 +3,6 @@
 		<td class="product-row-data">@{{ product.quantity }}</td>
 		<td class="product-row-data">@{{ product.name }}</td>
 		<td class="product-row-data">$@{{ (product.price / 100).toFixed(2) }}</td>
-		<td class="product-row-data"><span class="glyphicon glyphicon-minus-sign" v-on:click="subtractProduct(@{{ product }})"></span></td>
+		<td class="product-row-data"><span class="glyphicon glyphicon-minus-sign" v-on:click="subtractProduct(product)"></span></td>
 	</tr>
 </template>
