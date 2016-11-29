939cdf8b5287ce32682d97d334e9204b0861e15e
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-11-28 19:11:44 -0500

    updated how price is handled

--- resources/views/partials/transactions/bill_list.blade.php
+++ resources/views/partials/transactions/bill_list.blade.php
@@ -2,6 +2,6 @@
 	<tr class="product-row">
 		<td class="product-row-data">@{{ product.quantity }}</td>
 		<td class="product-row-data">@{{ product.name }}</td>
-		<td class="product-row-data">@{{ product.price }}</td>
+		<td class="product-row-data">@{{ product.price / 100 }}</td>
 	</tr>
 </template>
