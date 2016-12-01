1bb333e6989da43aa7410dd2416029b22f8a5c2c
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-01 11:14:04 -0500

    saving bill

--- resources/views/partials/transactions/save.blade.php
+++ resources/views/partials/transactions/save.blade.php
@@ -2,8 +2,8 @@
   <form action="{{ route('bill.update', ['transactionId' => $transaction->id]) }}" method="patch">
    	<input type="hidden" name="user_id" value="{{ $customer->id }}">
     <input type="hidden" name="paid" value="false">
-    <input type="hidden" name="products" value="@{{ JSON.stringify(this.bill) }}">
-    <input type="hidden" name="total" value="@{{ totalBill }}">
+    <input type="hidden" name="products" :value="JSON.stringify(bill)">
+    <input type="hidden" name="total" :value="totalBill">
     {{ csrf_field() }}
     <input type="submit" value="Keep Open" class="btn btn-block btn-primary btn-xs">
   </form>
