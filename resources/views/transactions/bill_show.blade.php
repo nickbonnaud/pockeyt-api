06446db614358f918890d99da14713ce7cdf0577
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-01 16:49:06 -0500

    saving bill

--- resources/views/transactions/bill_show.blade.php
+++ resources/views/transactions/bill_show.blade.php
@@ -119,7 +119,7 @@
         leaving: function() {
           console.log(this.saved);
           if(! this.saved) {
-            return alert("Leaving");
+            return "leaving";
           }
         }
       }
