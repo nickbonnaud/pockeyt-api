c84c65b3b7e1e044d4e370a47b77134725ec66b3
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-01 11:23:39 -0500

    saving bill

--- resources/views/transactions/bill_show.blade.php
+++ resources/views/transactions/bill_show.blade.php
@@ -58,7 +58,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.0.1/vue.js"></script>
   <script>
 
-    console.log(JSON.parse({{ $transaction->products }});
+    console.log(JSON.parse({!! $transaction->products !!});
 
     // var currentBill = {
     //   fetch: function() {
