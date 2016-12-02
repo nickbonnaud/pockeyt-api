1fcd53ebb6b261a50ad594820cd9d58a6d120545
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-02 07:57:58 -0500

    creating transactions

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -105,8 +105,8 @@ class TransactionsController extends Controller
 
     private function createCharge($transaction, $customer, $profile) {
         $amount = ($transaction->total) / 100;
-        dd($amount);
-        $serviceFee = $amount * 0.02;
+        $serviceFee = round($amount * 0.02, 2);
+        dd($serviceFee);
         $result = \Braintree_Transaction::sale([
             'merchantAccountId' => $profile->id,
             'amount' => $amount,
