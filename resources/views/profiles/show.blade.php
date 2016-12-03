ebca82211b75046bac191de4777ed15f649d5626
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-03 10:17:18 -0500

    removing user transactions

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -53,7 +53,7 @@
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title" id="CustomerinfoModal">@{{user.first_name}} @{{user.last_name}} | Recent purchases</h4>
+              <h4 class="modal-title" id="CustomerinfoModal">@{{user.first_name}} @{{user.last_name | setPossessive}} recent purchases</h4>
             </div>
             <div class="modal-body">
               <ul class="timeline">
