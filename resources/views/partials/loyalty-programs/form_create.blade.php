9b10b984529423fc34f41950c3700c26e1ff4068
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-06 07:09:02 -0500

    adding loyalty program

--- resources/views/partials/loyalty-programs/form_create.blade.php
+++ resources/views/partials/loyalty-programs/form_create.blade.php
@@ -15,14 +15,14 @@
 	</div>
 	<div class="form-group" v-if= "selection == 'increments'">
 		<label for="purchases_required">Number of Purchases required for reward</label>
-		<div class="input-group col-xs-4">
+		<div class="input-group col-xs-3 col-md-3">
 			<span class="input-group-addon">#</span>
 			<input class="form-control" type="number" name="purchases_required" id="purchases_required" placeholder="20">
 		</div>
 	</div>
 	<div class="form-group" v-if= "selection == 'amounts'">
 		<label for="amount_required">Total amount customers must spend to receive reward</label>
-		<div class="input-group col-xs-4">
+		<div class="input-group col-xs-3 col-md-3">
 			<span class="input-group-addon">$</span>
 			<input class="form-control" type="number" name="amount_required" id="amount_required" pattern="^\\$?(([1-9](\\d*|\\d{0,2}(,\\d{3})*))|0)(\\.\\d{1,2})?$" step="any" placeholder="50.00">
 		</div>
