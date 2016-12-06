74df18eddc61d012077adeb7d2e8b58eeb192b9b
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-06 03:11:47 -0500

    adding loyalty program

--- resources/views/loyalty-programs/create.blade.php
+++ resources/views/loyalty-programs/create.blade.php
@@ -24,25 +24,25 @@
 							<div class="form-group">
 								<div class="radio">
 									<label>
-										<input type="radio" name="optionsRadios" id="increments" value="increments">
+										<input type="radio" name="optionsRadios" id="increments" value="increments" v-model="selection">
 										Reward customers once they make a certain number of purchases
 									</label>
 								</div>
 								<div class="radio">
 									<label>
-										<input type="radio" name="optionsRadios" id="amounts" value="amounts">
+										<input type="radio" name="optionsRadios" id="amounts" value="amounts" v-model="selection">
 										Or reward customers after they have spent a certain amount
 									</label>
 								</div>
 							</div>
-							<div class="form-group">
+							<div class="form-group" v-if={{ selection == 'increments' }}>
 								<label for="purchases_required">Number of Purchases required for reward</label>
 								<div class="input-group col-xs-3">
 									<span class="input-group-addon">#</span>
 									<input class="form-control" type="number" name="purchases_required" id="purchases_required" placeholder="20">
 								</div>
 							</div>
-							<div class="form-group">
+							<div class="form-group" v-if={{ selection == 'amounts' }}>
 								<label for="amount_required">Total amount customers must spend to receive reward</label>
 								<div class="input-group col-xs-3">
 									<span class="input-group-addon">$</span>
