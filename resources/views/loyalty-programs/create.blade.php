d556d5667f0f5c86352e65f5486a04c1d797e77e
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-06 02:30:28 -0500

    adding loyalty program

--- resources/views/loyalty-programs/create.blade.php
+++ resources/views/loyalty-programs/create.blade.php
@@ -40,14 +40,14 @@
 								<span class="input-group-addon">
 									<i class="fa plus-square"></i>
 								</span>
-								<input type="number" name="purchases_required" id="purchases_required">
+								<input class="form-control" type="number" name="purchases_required" id="purchases_required">
 							</div>
 							<h4>Total amount customers must spend to receive reward</h4>
 							<div class="input-group">
 								<span class="input-group-addon">
 									<i class="fa fa-dollar"></i>
 								</span>
-								<input type="number" name="amount_required" id="amount_required" pattern="^\\$?(([1-9](\\d*|\\d{0,2}(,\\d{3})*))|0)(\\.\\d{1,2})?$" step="any">
+								<input class="form-control" type="number" name="amount_required" id="amount_required" pattern="^\\$?(([1-9](\\d*|\\d{0,2}(,\\d{3})*))|0)(\\.\\d{1,2})?$" step="any">
 							</div>
 						</div>
 					</div>
