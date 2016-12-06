42e649359bc9dda63b02592a77454bc5be3b14d1
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-06 07:02:13 -0500

    adding loyalty program

--- resources/views/loyalty-programs/create.blade.php
+++ resources/views/loyalty-programs/create.blade.php
@@ -20,36 +20,9 @@
 						<div class="box-header with-border">
 							<h3 class="box-title">Program Details</h3>
 						</div>
-						<div class="box-body">
-							<div class="form-group">
-								<div class="radio">
-									<label>
-										<input type="radio" name="optionsRadios" id="increments" value="increments" v-model="selection">
-										Reward customers once they make a certain number of purchases
-									</label>
-								</div>
-								<div class="radio">
-									<label>
-										<input type="radio" name="optionsRadios" id="amounts" value="amounts" v-model="selection">
-										Or reward customers after they have spent a certain amount
-									</label>
-								</div>
-							</div>
-							<div class="form-group" v-if= "selection == 'increments'">
-								<label for="purchases_required">Number of Purchases required for reward</label>
-								<div class="input-group col-xs-3">
-									<span class="input-group-addon">#</span>
-									<input class="form-control" type="number" name="purchases_required" id="purchases_required" placeholder="20">
-								</div>
-							</div>
-							<div class="form-group" v-if= "selection == 'amounts'">
-								<label for="amount_required">Total amount customers must spend to receive reward</label>
-								<div class="input-group col-xs-3">
-									<span class="input-group-addon">$</span>
-									<input class="form-control" type="number" name="amount_required" id="amount_required" pattern="^\\$?(([1-9](\\d*|\\d{0,2}(,\\d{3})*))|0)(\\.\\d{1,2})?$" step="any" placeholder="50.00">
-								</div>
-							</div>
-						</div>
+						{!! Form::open(['route' => 'loyalty-programs.store']) !!}
+							@include('partials.loyalty-programs.form_create')
+						{!! Form::close() !!}
 					</div>
 				</div>
 			</section>
