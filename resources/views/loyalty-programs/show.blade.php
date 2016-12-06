198e046a37c620c45faf07835430e5b55b9f323f
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-06 08:50:02 -0500

    adding loyalty program

--- resources/views/loyalty-programs/show.blade.php
+++ resources/views/loyalty-programs/show.blade.php
@@ -22,9 +22,9 @@
 						</div>
 						<div class="box-body">
 							@if($loyaltyProgram->is_increment)
-								<h4>Your current Loyalty Program requires {{ $loyaltyProgram->purchases_required }} purchases per reward.</h4>
+								Your current Loyalty Program requires <b>{{ $loyaltyProgram->purchases_required }}</b> purchases per reward.
 							@else
-								<h4>Your current Loyalty Program requires ${{ $loyaltyProgram->amount_required }}'s in total purchases per reward.</h4>
+								Your current Loyalty Program requires <b>${{ $loyaltyProgram->amount_required }}'s</b> in total purchases per reward.
 							@endif
 							<a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#LoyaltyProgramModal">
 		          	<b>Change</b>
