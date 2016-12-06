164b17bed1cf4c3d8ab4870f02335d992aeb1314
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-06 09:46:45 -0500

    adding loyalty program

--- resources/views/partials/loyalty-programs/delete.blade.php
+++ resources/views/partials/loyalty-programs/delete.blade.php
@@ -1,5 +1,5 @@
 @if($signedIn && ($isAdmin || $user->profile->owns($loyaltyProgram)))
-    <form action="{{ route('loyalty-program.destroy', ['loyaltyProgram' => $loyaltyProgram->id]) }}" method="post">
+    <form action="{{ route('loyalty-programs.destroy', ['loyaltyProgram' => $loyaltyProgram->id]) }}" method="post">
         <input type="hidden" name="_method" value="DELETE">
         {{ csrf_field() }}
         <input type="submit" value="Delete" class="btn btn-block btn-danger btn-sm">
