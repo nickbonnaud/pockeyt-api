98645cd97d5b6f28779b0c0407d31eb6c359d2ab
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-08 08:03:28 -0500

    changing loyalty program data

--- app/Http/Requests/UpdateTransactionRequest.php
+++ app/Http/Requests/UpdateTransactionRequest.php
@@ -3,36 +3,35 @@
 namespace App\Http\Requests;
 
 class UpdateTransactionRequest extends Request {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize() {
-        $isAuthorized = false;
-        if (!is_null($user = \Auth::user())) {
-            $transactions = \Auth::user()->profile->transactions->where('paid', '=', false);
-            foreach ($transactions as $transaction) {
-            	dd($this->route('transactionId'));
-                if ($transaction->id == $this->route('transactions')) {
-                    $isAuthorized = true;
-                }
-            }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize() {
+    $isAuthorized = false;
+    if (!is_null($user = \Auth::user())) {
+      $transactions = \Auth::user()->profile->transactions->where('paid', '=', false);
+      foreach ($transactions as $transaction) {
+        if ($transaction->id == $this->route('transactionId')) {
+          $isAuthorized = true;
         }
-        return $isAuthorized;
+      }
     }
+    return $isAuthorized;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules() {
-      return [
-        'profile_id' => 'required',
-        'user_id' => 'required',
-        'products' => 'required',
-        'total' => 'required'
-      ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules() {
+    return [
+      'profile_id' => 'required',
+      'user_id' => 'required',
+      'products' => 'required',
+      'total' => 'required'
+    ];
+  }
 }
