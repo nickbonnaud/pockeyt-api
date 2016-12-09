f316f17356fd7bef197794b69deeb9fdfa0d5bb6
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-09 18:31:38 -0500

    adding highlight for reedems

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -35,7 +35,7 @@
           <div class="box box-primary">
             <div class="box-header with-border text-center">
               <a class="customer-name-title" href="#" data-toggle="modal" data-target="#CustomerinfoModal">
-                <h3 class="box-title">@{{user.first_name}} @{{user.last_name}}</h3>
+                <h3 class="box-title">@{{user.last_name}}</h3>
               </a>
               <div class="box-body">
                 <a href="#" data-toggle="modal" data-target="#CustomerinfoModal">
