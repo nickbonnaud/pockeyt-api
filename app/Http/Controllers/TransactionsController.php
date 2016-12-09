3be27cf0a98d1144ce8fae9a973573c0cb6bfa32
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-09 14:09:10 -0500

    adding highlight for reedems

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -268,7 +268,7 @@ class TransactionsController extends Controller
         if(isset($redeemableDeal)) {
             return response()->json($redeemableDeal);
         } else {
-            return 'none';
+            return;
         }
     }
 
