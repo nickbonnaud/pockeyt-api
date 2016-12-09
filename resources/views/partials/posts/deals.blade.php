5ef2ec5eec94aa3e3a1a2210b9f776c4748edad8
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-09 08:51:53 -0500

    adding deals

--- resources/views/partials/posts/deals.blade.php
+++ resources/views/partials/posts/deals.blade.php
@@ -3,7 +3,7 @@
     <div class="box box-default">
       <div class="box-header with-border">
        <p class="pull-right">Expires on: <b>{{ $post->end_date }}</b></p>
-        <h3 class="box-title" ><a href="#" data-toggle="modal" data-target="#dealModal">{{ str_limit($post->message, 85) }}</a></h3>
+        <h3 class="box-title" v-on:click="getPurchasedDeals({{ $post }})"><a href="#" data-toggle="modal" data-target="#dealModal">{{ str_limit($post->message, 85) }}</a></h3>
       <i class="fa fa-calendar pull-right"></i>
       </div>
       <div class="box-body">
