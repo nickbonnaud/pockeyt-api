4132b35509c240567b21f57d593dc67465034ccd
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-11 11:04:37 -0500

    using ajax to get pruchases

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -44,7 +44,7 @@ class GeoController extends Controller
                     $redeemableDeal = Transaction::where(function($query) use ($dbUser, $business) {
                         $query->where('user_id', '=', $dbUser->id)
                             ->where('profile_id', '=', $business->id)
-                            ->where('redeemed', '=', false);     
+                            ->where('redeemed', '=', 0);     
                     })->first();
                     if (isset($redeemableDeal)) {
                         $user['redeemableDeal'] = $redeemableDeal->id;
