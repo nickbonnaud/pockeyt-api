6fc3d802497384e4409e0971c84ad56451aded81
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-13 00:57:17 -0500

    added deal_item to posts

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -113,7 +113,7 @@
                 <div class="box-body">
                   <div v-for="deal in deals">
                     <div v-if="deal.user_id === user.id">
-                      <h3 class="pull-left">@{{ deal.products }}</h3>
+                      <h3 class="pull-left">@{{ deal.products | json }}</h3>
                       <span class="pull-right">
                       <button class="btn btn-block btn-success btn-sm pull-right">Redeem!</button>
                       </span>
@@ -197,10 +197,6 @@
               return value.concat("'s");
             }
           },
-          parse: function(value) {
-            console.log(value);
-            return JSON.parse(value);
-          }
         },
 
         methods: {
