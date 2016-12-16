ef731885822248c280339e3d297e94e5a8a0ada1
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-16 01:48:43 -0500

    adding qb sync capes

--- app/Http/Controllers/QuickBookController.php
+++ app/Http/Controllers/QuickBookController.php
@@ -221,7 +221,7 @@ class QuickBookController extends Controller
 	      foreach ($unSynchedTransactions as $transaction) {
 	      	$invoiceService = new \QuickBooks_IPP_Service_Invoice();
 					$invoice = new \QuickBooks_IPP_Object_Invoice();
-	      	$invoice->setDueDate(date('Y-m-d', $transaction->updated_at->toDateString()));
+	      	$invoice->setDueDate($transaction->updated_at->toDateString());
 	      	$invoice->setPrivateNote('Pockeyt Sale Transaction ID # ' . $transaction->id);
 
 	      	$line = new \QuickBooks_IPP_Object_Line();
