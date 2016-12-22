960e7477248e4b51d0b9495e2cce4d9e9e858a80
Nick Bonnaud <nick.bonnaud@gmail.com>
2016-12-22 17:26:37 -0500

    Adding sales tax to QB

--- app/Http/Controllers/QuickBookController.php
+++ app/Http/Controllers/QuickBookController.php
@@ -385,7 +385,7 @@ class QuickBookController extends Controller
 							$transaction->qb_synced = true;
 							$transaction->save();
               $InvoiceService = new \QuickBooks_IPP_Service_Invoice();
-              $invoices = $InvoiceService->query($Context, $realm, "SELECT * FROM Invoice STARTPOSITION 1 MAXRESULTS 10");
+              $invoices = $InvoiceService->query($this->context, $this->realm, "SELECT * FROM Invoice STARTPOSITION 1 MAXRESULTS 10");
               dd($invoices);
 						}
 						else
