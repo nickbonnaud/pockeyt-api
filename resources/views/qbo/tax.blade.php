1a2409e26cb29524ff4f636bd8208fd96ee9440b
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-05 08:08:38 -0500

    saving taxcode

--- resources/views/qbo/tax.blade.php
+++ resources/views/qbo/tax.blade.php
@@ -1,9 +1,9 @@
 @extends('layoutDashboard')
 @section('content')
 <div class="content-wrapper">
-	<section class="content">
-		<div>
-			<b>Please Adjust your Sales Tax Rate in QuickBoooks or Pockeyt.</b>
+	<section>
+		<div style="text-align: center">
+			<h4>Please Adjust your Sales Tax Rate in QuickBoooks or Pockeyt.</h4>
 			<button type="button" class="btn btn-block btn-primary btn-lg">Close Window</button>
 		</div>
 	</section>
