9cbbc3e6f4c5693804629996bd108f730521b017
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-06 19:22:05 -0500

    connecting square sync

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -123,6 +123,7 @@ class ProductsController extends Controller {
   public function getAccessToken($code) {
     $client = new \GuzzleHttp\Client(['base_uri' => 'https://connect.squareup.com/oauth2']);
     try {
+      dd(env('SQUARE_ID'));
       $response = $client->request('POST', '/token', [
         'json' => ['client_id' => env('SQUARE_ID'), 'client_secret' => env('SQUARE_SECRET'), 'code'=> $code]
       ]);
