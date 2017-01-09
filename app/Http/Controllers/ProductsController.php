40cf11b2aad0ddec7c2d2b40ed3bad868a8fdabf
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-09 12:36:41 -0500

    adding square oauth

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -107,10 +107,9 @@ class ProductsController extends Controller {
   }
 
   public function getAccessToken($code) {
-    dd($code);
-    $client = new \GuzzleHttp\Client(['base_uri' => 'https://connect.squareup.com/oauth2']);
+    $client = new \GuzzleHttp\Client(['base_uri' => 'https://connect.squareup.com/oauth2/']);
     try {
-      $response = $client->request('POST', '/token', [
+      $response = $client->request('POST', 'token', [
         'json' => ['client_id' => env('SQUARE_ID'),
         'client_secret' => env('SQUARE_SECRET'),
         'code'=> $code]
