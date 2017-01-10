a9c9ae0b49a07ae7b8dcc125d5aa31596058dd84
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-10 09:29:52 -0500

    adding square oauth

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -233,6 +233,7 @@ class ProductsController extends Controller {
     $product->price = $variation->price_money->amount;
     $product->sku = $variation->sku;
     $product->square_id = $variation->id;
+    dd($product);
     return $product->save();
   }
 
@@ -241,7 +242,7 @@ class ProductsController extends Controller {
     $product->price = $variation->price_money->amount;
     $product->sku = $variation->sku;
     $product->square_id = $variation->id;
-    dd($product);
+
     return $product->save();
   }
 
