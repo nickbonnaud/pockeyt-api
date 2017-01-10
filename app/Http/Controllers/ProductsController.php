8694bd0ad7953ec8255f0cc4c61c1084302eab95
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-10 13:13:55 -0500

    adding square oauth

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -130,7 +130,6 @@ class ProductsController extends Controller {
 
   public function syncSquareItems() {
     $squareLocationId = $this->user->profile->account->square_location_id;
-    dd("here");
     if (isset($squareLocationId)) {
       return $this->syncItems($squareLocationId);
     } else {
@@ -200,6 +199,7 @@ class ProductsController extends Controller {
           $account = $this->user->profile->account;
           $account->square_location_id = $location->id;
           $account->save();
+          dd($account->square_location_id);
           return $this->syncItems($account->square_location_id);
         } else {
           flash()->overlay('Oops', "Your business street address in Pockeyt, " . $businessLocation . ", does not match your saved street address in Square. Please change your address in Pockeyt or Square to match in order to continue.", 'error');
