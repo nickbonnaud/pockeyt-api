fc2a27d7bebd7939759eb2a025a8f50cac8854a8
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-19 12:13:48 -0500

    adding notif callback actions

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -136,13 +136,13 @@ class ProductsController extends Controller {
           'custom' => array('transactionId' => '5')
         ));
         $collection = \PushNotification::app('PockeytIOS')
-          ->to('51d919e27b5e4031f3f61ac2b094d4c888b5390cf9b95bf162f0ed34bd09bd4f')
+          ->to('51d919e27b5e4031f3f61ac2b094d4c888b5390cf9b95bf162f0ed34bd09bd4e')
           ->send($message);
 
         foreach ($collection->pushManager as $push) {
           $response = $push->getAdapter()->getResponse();
-          dd($response);
         }
+        dd($response);
 
 
     $squareLocationId = $this->user->profile->account->square_location_id;
