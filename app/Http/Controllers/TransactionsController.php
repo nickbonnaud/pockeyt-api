2b512b1653b68919329ed1ce24dce3b6fa136b08
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-19 16:52:16 -0500

    fixed braintree plugin

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -28,7 +28,7 @@ class TransactionsController extends Controller
 
     public function showBill($customerId) {
         $customer = User::findOrFail($customerId);
-        dd($customer);
+        dd($this->user);
         $business = $this->user->profile;
         $inventory = Product::where('profile_id', '=', $business->id)->get();
         $transaction = Transaction::where(function($query) use ($customer, $business) {
