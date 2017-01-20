40b87713f61d0fda0ef9017e1eedbb6ebd5980c4
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-20 09:15:32 -0500

    fixed braintree plugin

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -173,6 +173,8 @@ class TransactionsController extends Controller
 
     public function userConfirmBill(Request $request) {
         $authUser = JWTAuth::parseToken()->authenticate();
+        return response()->json(compact('authUser'));
+        dd('something');
         $transaction = Transaction::findOrFail($request->transactionId);
 
         if ($authUser->id === $transaction->user_id && !$transaction->paid) {
