fdf55b2d92034510b48381ec46f6f05f25d7470a
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-25 20:46:21 -0500

    displaying pending transactions

--- resources/views/layoutDashboard.blade.php
+++ resources/views/layoutDashboard.blade.php
@@ -288,7 +288,6 @@
             "positionClass": "toast-top-right",
             "preventDuplicates": false,
             "onclick": null,
-            "showDuration": "300",
             "hideDuration": "1000",
             "timeOut": 0,
             "extendedTimeOut": 0,
