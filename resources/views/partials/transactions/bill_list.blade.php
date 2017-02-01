7684c5fdd5a0e5d879dc9c772e86bacd7cca05c1
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-01-31 21:49:03 -0500

    adding css styling

--- resources/views/partials/transactions/bill_list.blade.php
+++ resources/views/partials/transactions/bill_list.blade.php
@@ -1,5 +1,5 @@
 <template v-for="product in bill">
-	<tr class="product-row">
+	<tr class="product-row" v-cloak>
 		<td class="product-row-data">@{{ product.quantity }}</td>
 		<td class="product-row-data">@{{ product.name }}</td>
 		<td class="product-row-data">$@{{ (product.price / 100).toFixed(2) }}</td>
