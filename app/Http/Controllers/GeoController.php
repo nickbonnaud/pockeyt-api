0b297ee64f8bcb86d6e5f19cb4fe7838adb1bc78
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-02-02 11:58:59 -0500

    changing geo controller

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -29,7 +29,7 @@ class GeoController extends Controller
         foreach ($geoData as $data) {
             $newData = $data;
         }
-        $user = $newData;
+        $user = $newData->latitude;
     	// $user['lat'] = $request->latitide;
     	// $user['lng'] = $request->longitude;
     	// $user['accuracy'] = $request->accuracy;
