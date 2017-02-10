004fdf213b7badec05769ef7ec304eae5e1f5bf1
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-02-10 15:12:38 -0500

    changing geo calcs

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -46,7 +46,7 @@ class GeoController extends Controller
         $geoData = $request->all();
 
         $business = 113;
-        $user = $geoData->payload;
+        $user = gettype($geoData);
         event(new CustomerEnterRadius($user, $business));
         $geoFenceEvent = $geoData->action;
 
