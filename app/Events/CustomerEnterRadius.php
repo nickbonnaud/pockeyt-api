606ddb6dee89003eeba0a925375df7729a5ed7f4
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-02-10 17:27:42 -0500

    changing geo calcs

--- app/Events/CustomerEnterRadius.php
+++ app/Events/CustomerEnterRadius.php
@@ -31,7 +31,7 @@ class CustomerEnterRadius extends Event implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        $channel = 'business' . $this->business->id;
+        $channel = 'business' . $this->business;
         return [$channel];
     }
 }
