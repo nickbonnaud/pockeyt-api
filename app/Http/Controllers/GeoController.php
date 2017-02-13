314abbff03ebf34c416e3c4af8239fb0b650fa64
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-02-13 17:49:08 -0500

    changing geo calcs

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -49,7 +49,7 @@ class GeoController extends Controller
 
         $business = 113;
         $user = $data;
-        event(new CustomerEnterRadius($user, $business));
+        event(new CustomerLeaveRadius($user, $business));
         $user = $userT;
 
         if (!$isHeartBeat) {
