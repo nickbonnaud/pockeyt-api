3a34b4aa462fc6d692a6956fcace0524d73a33cb
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-02-14 13:16:09 -0500

    created token column

---
+++ database/migrations/2017_02_14_131136_add_push_token_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPushTokenToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('push_token')->nullable()->after('customer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('push_token');
+        });
+    }
+}
