54a61408f8d102833f64cc0f8ff2525d77cad566
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-02-17 10:29:28 -0500

    uploading user photo

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,7 +16,6 @@ class UsersController extends Controller
     public function __construct()
     {
         parent::__construct();
-        $this->middleware('jwt.auth', []);
     }
 
     public function getAuthenticatedUser()
