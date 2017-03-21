7fb1fd57bb38032b8177e8391825e5d6e81610da
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-03-21 01:03:11 -0400

    adjusting deals api

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -503,7 +503,7 @@ class TransactionsController extends Controller
                 $join->on('transactions.deal_id', '=', 'posts.id')
                     ->where('transactions.user_id', '=', $user->id);
             })
-            ->orderBy('end_date', 'desc')->paginate(20);
+            ->orderBy('end_date', 'desc')->paginate(10);
 
             $transactions = $paginator->getCollection();
             return fractal()
