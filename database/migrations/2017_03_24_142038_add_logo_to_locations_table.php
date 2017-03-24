2be0a9f6c2949297a9387d0f65e767a34d15bdaf
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-03-24 14:27:15 -0400

    updating current location

---
+++ database/migrations/2017_03_24_142038_add_logo_to_locations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLogoToLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->string('business_logo')->after('location_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->dropColumn('business_logo');
+        });
+    }
+}
