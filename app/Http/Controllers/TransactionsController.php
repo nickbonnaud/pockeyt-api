2b96a561ab0c3368bdb8f7d1a1be6367a02d7ca4
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-03-30 10:00:29 -0400

    added analytics method and routes

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -536,9 +536,10 @@ class TransactionsController extends Controller
           $query->whereBetween('viewed_on', [$fromDate, $currentDate])
             ->where('user_id', '=', $customer->id)
             ->where('business_id', '=', $profile->id);
-        })->orderBy('viewed_on', 'desc')->first();
+        })->orderBy('viewed_on', 'desc')->take(1)->get();
 
-        return response($postViewed);
+        $result = isset($postViewed);
+        return response($result);
 
         if (isset($postViewed)) {
             $post = Post::findOrFail($postViewed->post_id);
