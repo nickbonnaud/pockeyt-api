4dc2f7a009404e718dcb32d81fe72515b5c4ebe4
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-03-30 09:53:34 -0400

    added analytics method and routes

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -538,7 +538,7 @@ class TransactionsController extends Controller
             ->where('business_id', '=', $profile->id);
         })->orderBy('viewed_on', 'desc')->take(1)->get();
 
-        return response($postViewed == 'null');
+        return response($postViewed);
 
         if (isset($postViewed)) {
             $post = Post::findOrFail($postViewed->post_id);
