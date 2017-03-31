530e84ab11b4cbb8ae0891901b0b765ea511eb67
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-03-30 22:51:51 -0400

    added analytics method and routes

--- resources/views/analytics/show.blade.php
+++ resources/views/analytics/show.blade.php
@@ -111,13 +111,9 @@
 					labels: labels,
 					datasets: [
 						{
-							label: "Interactions",
-							fillColor: "rgba(210, 214, 222, 1)",
-							strokeColor: "rgba(210, 214, 222, 1)",
-          		pointColor: "rgba(210, 214, 222, 1)",
-          		pointStrokeColor: "#c1c7d1",
-          		pointHighlightFill: "#fff",
-          		pointHighlightStroke: "rgba(220,220,220,1)",
+							label: "Views, Shares, Bookmarks",
+							backgroundColor: "rgba(52, 152, 219,1.0)",
+							hoverBorderColor: "rgba(41, 128, 185,1.0)",
           		data: data
 						}
 					]
