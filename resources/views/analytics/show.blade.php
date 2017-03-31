080664726fbe0dc8b07d8c6ec397f94eb659f131
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-03-31 12:37:08 -0400

    added analytics method and routes

--- resources/views/analytics/show.blade.php
+++ resources/views/analytics/show.blade.php
@@ -143,6 +143,7 @@
 
     	var lineInteractionsDay = $("#lineInterDay").get(0).getContext("2d");
     	var type = "interaction";
+    	console.log(this.postsActivityByDay);
     	var lineInteractionsDayData = this.formatLineData(this.postsActivityByDay, type);
     	var lineChartInter = new Chart(lineInteractionsDay, {
     		type: 'line',
