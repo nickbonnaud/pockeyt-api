8f38fbb85d1ea78d56f783440ef5a4a1d716023c
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-03-31 18:11:37 -0400

    added analytics method and routes

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -42,12 +42,12 @@ class AnalyticsController extends Controller
     for ($i = 0; $i <= 6; $i++) {
       $activityPerDayTotal = PostAnalytic::where(function($query) use ($profile, $i) {
         $query->where('business_id', '=', $profile->id)
-          ->whereRaw('WEEKDAY(updated_at) = ?', [3]);
+          ->whereRaw('WEEKDAY(updated_at) = ?', [$i]);
       })->count();
 
       $days = PostAnalytic::where(function($query) use ($profile, $i) {
         $query->where('business_id', '=', $profile->id)
-          ->whereRaw('WEEKDAY(updated_at) = ?', [3]);
+          ->whereRaw('WEEKDAY(updated_at) = ?', [$i]);
       })->groupBy(DB::raw('Date(updated_at)'))->count();
 
       if ($days !== 0) {
