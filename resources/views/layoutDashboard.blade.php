e18b3d27530017b70929daa3057b6f8f204a7d1d
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-04 19:35:23 -0400

    added analytics method and routes

--- resources/views/layoutDashboard.blade.php
+++ resources/views/layoutDashboard.blade.php
@@ -269,7 +269,10 @@
     var tab = new Vue({
       el: '#tab',
 
-      
+      data: {
+        transactionsPending: [],
+        transactionsFinalized: []
+      },
 
       mounted: function() {
         var pusher = new Pusher('f4976d40a137b96b52ea', {
