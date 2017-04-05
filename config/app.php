453fa44ba6c0a7083d3dc853682485ff63e4add6
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-05 17:04:06 -0400

    updating session open

--- config/app.php
+++ config/app.php
@@ -158,6 +158,7 @@ return [
         Oureastudios\Laravel\BraintreeServiceProvider::class,
         \SocialiteProviders\Manager\ServiceProvider::class,
         Davibennun\LaravelPushNotification\LaravelPushNotificationServiceProvider::class,
+        GeneaLabs\LaravelCaffeine\Providers\LaravelCaffeineService::class,
 
     ],
 
--- config/session.php
+++ config/session.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'lifetime' => 2,
+    'lifetime' => 7,
 
     'expire_on_close' => false,
 
