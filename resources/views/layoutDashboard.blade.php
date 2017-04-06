f73d590997a6173d3d89f58d6ff38b3aecd9a75a
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-06 02:57:26 -0400

    updating caffeine

--- resources/views/layoutDashboard.blade.php
+++ resources/views/layoutDashboard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta property="og:site_name" content="Pockeyt" />
     <title>Pockeyt Business</title>
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
