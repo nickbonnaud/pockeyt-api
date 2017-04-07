83389d83028462663a760330b1bf4b31cfb7e64c
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-07 02:17:15 -0400

    updating caffeine

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -470,35 +470,20 @@ class TransactionsController extends Controller
             $query->where('user_id', '=', $customerId)
                 ->where('business_id', '=', $businessId);
         })->orderBy('updated_at', 'desc')->first();
-        if (!isset($lastPostViewed)) {
-            $lastPostViewed = ["none"];
-
-        }
 
         $recentShared = PostAnalytic::where(function($query) use ($fromDate, $currentDate, $customerId, $businessId) {
             $query->where('user_id', '=', $customerId)
                 ->where('business_id', '=', $businessId)
                 ->whereBetween('shared_on', [$fromDate, $currentDate]);
         })->orderBy('shared_on', 'desc')->first();
-        return response()->json($recentShared);
-        if (!isset($recentShared)) {
-            $recentShared = ["none"];
-        }
-
+    
         $recentBookmarked = PostAnalytic::where(function($query) use ($fromDate, $currentDate, $customerId, $businessId) {
             $query->where('user_id', '=', $customerId)
                 ->where('business_id', '=', $businessId)
                 ->whereBetween('bookmarked_on', [$fromDate, $currentDate]);
         })->orderBy('bookmarked_on', 'desc')->first();
-        if (!isset($recentBookmarked)) {
-            $recentBookmarked = ["none"];   
-        }
 
-        if(isset($purchases)) {
-            return response()->json($purchases, $lastPostViewed, $recentShared, $recentBookmarked);
-        } else {
-            return response()->json($lastPostViewed, $recentShared, $recentBookmarked);
-        }
+        return response()->json($purchases, $lastPostViewed, $recentShared, $recentBookmarked);
     }
 
     public function getUserDeals(Request $request) {
