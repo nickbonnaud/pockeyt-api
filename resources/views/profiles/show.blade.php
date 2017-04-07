f0a2f870623053c4efa87c50d9ffceef8946bd69
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-07 13:02:45 -0400

    updating caffeine

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -212,7 +212,7 @@
         props: ['products'],
         template: '<div><div v-for="item in items"><p class="timeline-purchases-left">@{{ item.quantity }} x @{{ item.name }}</p><p class="timeline-purchases-right">$@{{ (item.price / 100) }}</p></div></div>',
         data: function() {
-          console.log(this.products);
+          console.log("hello");
           return {
             items: JSON.parse(this.products)
           }
