87e32c2c31ea685d683d62f4ff7904352019c37c
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-07 12:00:42 -0400

    updating caffeine

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -69,7 +69,7 @@
 
                         <div class="info-box-content">
                           <span class="info-box-text">Date Last Purchase</span>
-                          <span v-if="purchases" class="info-box-number">@{{lastPurchase.updated_at}}</span>
+                          <span v-if="purchases" class="info-box-number">@{{ lastPurchase.updated_at | setDate }}</span>
                           <span v-else class="info-box-number">No Recent</span>
                         </div>
                       </div>
