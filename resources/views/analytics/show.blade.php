0f011d8c1546a81a4adaaa311624a053bcd95acc
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-09 13:41:22 -0400

    updating caffeine

--- resources/views/analytics/show.blade.php
+++ resources/views/analytics/show.blade.php
@@ -390,7 +390,7 @@
 						xAxes: [{
 							ticks: {
 								callback: function(value, index, values) {
-									console.log(moment(value).format('MMM D YY'));
+									console.log(Date(value).format('MMM D YY'));
                   
                 }
 							},
