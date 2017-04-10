188397bfa7a4a645a29fa3af1ce7a1248a1c3b68
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-10 19:58:33 -0400

    updating y axis values if no return value

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -131,10 +131,11 @@
 
                         <div class="info-box-content">
                           <span class="info-box-text">Last Viewed Post</span>
-                          <span class="info-box-number">@{{ lastViewedPost.updated_at | setDateTime }}</span>
+                          <span v-if="!lastViewedPost.updated_at" class="info-box-number">No Recent</span>
+                          <span v-if="lastViewedPost.updated_at" class="info-box-number">@{{ lastViewedPost.updated_at | setDateTime }}</span>
                         </div>
                       </div>
-                      <div class="box box-success collapsed-box">
+                      <div v-if="lastViewedPost.updated_at" class="box box-success collapsed-box">
                         <div class="box-header with-border">
                           <i class="fa fa-eye"></i>
                           <h3 class="box-title">Post Details</h3>
