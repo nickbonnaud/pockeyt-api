80c4c4fe60d98a3d22a89a68576db505231ad966
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-10 17:52:12 -0400

    updating y axis values if no return value

--- resources/views/analytics/show.blade.php
+++ resources/views/analytics/show.blade.php
@@ -293,6 +293,7 @@
     		ticks: {
     			beginAtZero: true,
     			callback: function(value, index, values) {
+    				console.log(value);
             if(parseInt(value) >= 1000){
               return '$' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             } else {
