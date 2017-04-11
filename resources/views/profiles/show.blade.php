5855ba5ef1a218e0828e24258ba731318e447243
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-11 00:12:11 -0400

    updating y axis values if no return value

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -443,7 +443,7 @@
               var point = {x: purchase.updated_at, y: 0};
               purchasesData.push(point);
             });
-
+            purchasesData.push(moment().format());
             var purchaseHistoryChart = new Chart(purchaseHistoryCanvas, {
               type: 'line',
               data: {
@@ -451,8 +451,8 @@
                   data: purchasesData,
                   pointRadius: 5,
                   borderColor: "rgba(52, 152, 219,1.0)",
-                  pointBorderColor: "#f39c12",
-                  pointBackgroundColor: "#f39c12",
+                  pointBorderColor: ["#f39c12", "#f39c12", "#f39c12", "#f39c12", "#f39c12", "#00a65a"],
+                  pointBackgroundColor: ["#f39c12", "#f39c12", "#f39c12", "#f39c12", "#f39c12", "#00a65a"],
                 }]
               },
               options: {
