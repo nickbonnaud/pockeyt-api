1a9118132f74312e3c1aa2ba1888478b12102129
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-11 00:23:58 -0400

    updating y axis values if no return value

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -452,8 +452,8 @@
                   data: purchasesData,
                   pointRadius: 5,
                   borderColor: "rgba(52, 152, 219,1.0)",
-                  pointBorderColor: "#f39c12",
-                  pointBackgroundColor: "#f39c12",
+                  pointBorderColor: ["#f39c12", "#f39c12", "#f39c12", "#f39c12", "#f39c12", "#00a65a"],
+                  pointBackgroundColor: ["#f39c12", "#f39c12", "#f39c12", "#f39c12", "#f39c12", "#00a65a"],
                 }]
               },
               options: {
