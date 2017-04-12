c652c9fa710fd7769f5e9414fec3fd26abbbf61c
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-12 15:10:47 -0400

    updating email receipt

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -7,6 +7,7 @@ Use Illuminate\HttpResponse;
 use App\Http\Requests;
 use App\Transaction;
 use Mail;
+use App\User;
 use App\Http\Controllers\Controller;
 
 class EmailController extends Controller
@@ -19,6 +20,7 @@ class EmailController extends Controller
 	public function show() {
 		$profile = $this->user->profile;
 		$transaction = Transaction::where('profile_id', '=', $profile->id)->first();
+		$customer = User::findOrFail($transaction->user_id);
 		$items = $transaction->products;
 		$items = json_decode($items);
 
