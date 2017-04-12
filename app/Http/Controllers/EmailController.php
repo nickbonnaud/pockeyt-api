b6bfd69a96a8f77d9988783b22a63f31e5017501
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-12 17:12:13 -0400

    updating email receipt

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 Use Illuminate\HttpResponse;
 use App\Http\Requests;
 use App\Transaction;
+use Mail;
+use App\User;
 use App\Http\Controllers\Controller;
 
 class EmailController extends Controller
@@ -18,9 +20,13 @@ class EmailController extends Controller
 	public function show() {
 		$profile = $this->user->profile;
 		$transaction = Transaction::where('profile_id', '=', $profile->id)->first();
+		$customer = User::findOrFail($transaction->user_id);
 		$items = $transaction->products;
 		$items = json_decode($items);
 
-		return view('emails.receipt', compact('transaction', 'items', 'profile'));
+	return Mail::send('emails.receipt', ['items' => $items, 'profile' => $profile, 'transaction' => $transaction], function($m) use ($customer, $profile) {
+          $m->from('receipts@pockeyt.com', 'Pockeyt Receipts');
+          $m->to($customer->email, $customer->first_name)->subject('Recent transaction from Pockeyt');
+      });
 	}
 }
