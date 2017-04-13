643a01a7c6695a1ea9d99cea3b23813ee2333545
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-13 10:39:45 -0400

    updating email receipt

--- resources/views/transactions/bill_create.blade.php
+++ resources/views/transactions/bill_create.blade.php
@@ -80,7 +80,7 @@
                       <input v-model="price" type="number" name="price" class="form-control" style="width: 50%;" id="inputPrice" placeholder="Price" pattern="^\\$?(([1-9](\\d*|\\d{0,2}(,\\d{3})*))|0)(\\.\\d{1,2})?$" step="any" required>
                     </div>
                   </div>
-                  <button v-bind:disabled="(name == '' && price == '')" type="submit" class="btn btn-block bg-olive" v-on:click="addCustomProduct()">Add</button>
+                  <button v-bind:disabled="(name == '' || price == '')" type="submit" class="btn btn-block bg-olive" v-on:click="addCustomProduct()">Add</button>
                 </form>
               </div>
             </section>
