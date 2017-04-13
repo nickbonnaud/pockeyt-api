67025b824dbedc0966cb884a1de6cf5ca8e9eccf
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-13 12:10:11 -0400

    updating email receipt

--- resources/views/partials/transactions/inventory.blade.php
+++ resources/views/partials/transactions/inventory.blade.php
@@ -1,6 +1,6 @@
 <div v-if="inventory.length > 0" >
 	<div class="col-md-3" v-for="product in inventory">
-    <div class="box-inventory" v-on:click="addProduct({{ $product }})">
+    <div class="box-inventory" v-on:click="addProduct()">
       <div class="box-body-inventory">
         <img v-if="product.product_tn_photo_path" :src="product->product_tn_photo_path">
         <img v-else src="@{{ asset('/images/noImage.png') }}">
