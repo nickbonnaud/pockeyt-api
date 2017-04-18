b7f37ff1d5882629bad9693c8ed2af6b494442bc
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-18 16:09:13 -0400

    allow geo to be nullable

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -48,7 +48,7 @@ class ProfilesController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function create() {
-        if(!is_null($this->user->profile))
+        if(!is_null($this->user->profile->geoLocation))
             return redirect()->route('profiles.show', ['profiles' => $this->user->profile->id]);
 
         $tags = \App\Tag::lists('name', 'id');
