c8a9144f4ea4dcd934e44c991509a8e8c19396e2
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-18 22:24:17 -0400

    allow geo to be nullable

--- resources/views/transactions/bill_create.blade.php
+++ resources/views/transactions/bill_create.blade.php
@@ -169,8 +169,8 @@
         },
 
         addProduct: function(product) {
+          console.log(product);
           var bill = this.bill;
-          console.log("hello");
           var result = $.grep(bill, function(item) { return item.id === product.id});
           if (result.length === 0) {
             product['quantity'] = 1;
