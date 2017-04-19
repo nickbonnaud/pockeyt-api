1f4623ba0f679b4d6bf9ae3c38f654eeb7da42f6
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-19 09:46:03 -0400

    allow geo to be nullable

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -193,6 +193,12 @@
                     {!! Form::close() !!}
               </div>
             </div>
+            <div class="modal-footer">
+                <div class="form-group">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
