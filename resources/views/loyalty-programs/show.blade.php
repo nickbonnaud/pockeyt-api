4a77ecb221b73d946fc135c7a704a10a870c3c15
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-19 11:38:06 -0400

    allow geo to be nullable

--- resources/views/loyalty-programs/show.blade.php
+++ resources/views/loyalty-programs/show.blade.php
@@ -81,7 +81,7 @@
 					</div>
 	      </div>
 	      <div class="modal-footer">
-				  <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+				  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 				  @include('partials.loyalty-programs.delete')
 				</div>
 	    </div>
