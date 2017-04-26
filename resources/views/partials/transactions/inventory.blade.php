098c6d79c173667a4c94d7f78e67e3739aae05dd
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-26 01:31:28 -0400

    adjust styling of navbar

--- resources/views/partials/transactions/inventory.blade.php
+++ resources/views/partials/transactions/inventory.blade.php
@@ -1,5 +1,5 @@
 <div v-if="inventory.length > 0" >
-	<div class="col-md-3 col-sm-4 col-xs-2" v-for="product in productsFilter">
+	<div class="col-md-3 col-sm-6 col-xs-6" v-for="product in productsFilter">
     <div class="box-inventory" v-on:click="addProduct(product)">
       <div class="box-body-inventory">
         <img v-if="product.product_tn_photo_path" :src="product.product_tn_photo_path">
