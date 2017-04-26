ef1860f275c7d9cb5302e5157f2bde7634e5aa04
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-25 20:17:04 -0400

    adjust styling of navbar

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -531,7 +531,7 @@
                   xAxes: [{
                     ticks: {
                       callback: function(value, index, values) {
-                        console.log(moment(value).format());
+                        console.log(value);
                         var formattedTick = (moment(value).format('MMM D YY'));
                         var checkDate = null;
                         purchasesData.forEach(function(date) {
