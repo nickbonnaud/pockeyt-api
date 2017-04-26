12c35dfc7c2dccc15797a463c3aed7e669478466
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-26 10:51:33 -0400

    adjust styling of navbar

--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -31,7 +31,7 @@
         </ol>
       </section>
     	<section class="content">
-      	<div class="col-md-6">
+      	<div class="col-md-6 col-sm-6 col-xs-12">
       		<div class="box box-primary">
       			<div class="box-header with-border">
       				<h3 class="box-title">Create a New Post</h3>
@@ -42,7 +42,7 @@
       				{!! Form::close() !!}
       		</div>
       	</div>
-        <div class="scroll-container col-md-6">
+        <div class="scroll-container col-md-6 col-sm-6 col-xs-12">
             <div class="scroll-contents">
               @include('partials.posts.list', ['posts' => $posts, 'no_icons' => true])
             </div>
