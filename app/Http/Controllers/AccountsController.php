4bc47231315a3b4a8e6f08dec63854cf849e9b47
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-27 12:53:25 -0400

    adding currency input mask

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -205,10 +205,7 @@ class AccountsController extends Controller
             \Braintree_WebhookNotification::SUB_MERCHANT_ACCOUNT_APPROVED,
             'my_id'
         );
-        $notification = \Braintree_WebhookNotification::parse(
-            $sampleNotification->bt_signature, $sampleNotification->bt_payload
-        );
-        dd($notification);
+        dd($sampleNotification->bt_signature);
         if (isset($sampleNotification->bt_signature) && isset($sampleNotification->bt_payload)) {
             $notification = \Braintree_WebhookNotification::parse(
                 $sampleNotification->bt_signature, $sampleNotification->bt_payload
