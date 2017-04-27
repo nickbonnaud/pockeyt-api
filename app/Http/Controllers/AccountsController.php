3460797c79f77f82db79b65c41df574ca9fa9009
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-27 11:44:39 -0400

    adding currency input mask

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -182,6 +182,7 @@ class AccountsController extends Controller
 
     public function postStatus(Request $request)
     {
+        return response("ok");
         if (isset($request->bt_signature) && isset($request->bt_payload)) {
             $notification = \Braintree_WebhookNotification::parse(
                 $request->bt_signature, $request->bt_payload
