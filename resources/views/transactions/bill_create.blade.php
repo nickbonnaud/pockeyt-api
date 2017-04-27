22a722dd9bc83543aad58e7347d760ded0cedffc
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-27 10:20:08 -0400

    adding currency input mask

--- resources/views/transactions/bill_create.blade.php
+++ resources/views/transactions/bill_create.blade.php
@@ -83,7 +83,7 @@
                   <div class="form-group" style="margin-left: 15%;">
                     <label for="inputPrice" class="col-sm-2 control-label">Price</label>
                     <div class="col-sm-10">
-                      <input v-model="price" type="tel" name="price" class="currency" style="width: 50%;" id="inputPrice" pattern="^\\$?(([1-9](\\d*|\\d{0,2}(,\\d{3})*))|0)(\\.\\d{1,2})?$" step="any" required>
+                      <input type="text" name="price" class="currency" style="width: 50%;" id="inputPrice" required>
                     </div>
                   </div>
                   <button v-bind:disabled="(name == '' || price == '')" type="button" class="btn btn-block btn-primary" v-on:click="addCustomProduct()">Add</button>
