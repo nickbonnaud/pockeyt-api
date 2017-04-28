5d8e569a5136111da6a9c890a0b3b701eb47bbe7
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-04-28 15:16:51 -0400

    adding end user agreement

--- resources/views/app/qbDisconnect.blade.php
+++ resources/views/app/qbDisconnect.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 	<h3>Successfully Disconnected Pockeyt</h3>
 	<p>Pockeyt is no longer connected to your Quickbooks account and will no longer auto-update transactions.</p>
-	<p>To reconnect, please go to the <a href="{{ route('app.index') }}" target="_blank">Pockeyt Dashboard</a> and reconnect Quickbooks in your Inventory tab.</p>
+	<p>To reconnect, please go to the <a href="{{ route('app.index') }}">Pockeyt Dashboard</a> and reconnect Quickbooks in your Inventory tab.</p>
 	
 @stop
 
