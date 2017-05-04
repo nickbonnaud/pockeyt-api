65a831aedf066cdacf372778ba68f46ce36e88f3
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-04 10:21:53 -0400

    updating QB OID

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -169,6 +169,7 @@ Route::group(['prefix' => 'api'], function() {
 
 //Quickbook Routes
 Route::get('qbo/openid', 'QuickBookController@qboOpenId');
+Route::post('qbo/openid', 'QuickBookController@qboOpenId');
 Route::get('qbo/oauth','QuickBookController@qboOauth');
 Route::get('qbo/success','QuickBookController@qboSuccess');
 Route::get('qbo/tax', 'QuickBookController@qboTax')->name('qbo.tax');
