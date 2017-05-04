ef8dd3e851d898378624b00efa3169f9a4436cc7
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-04 11:25:53 -0400

    updating QB OID

--- app/Http/Controllers/QuickBookController.php
+++ app/Http/Controllers/QuickBookController.php
@@ -32,8 +32,7 @@ class QuickBookController extends Controller
     $openid = new \LightOpenID('https://pockeyt-test.com');
     if (!$openid->mode) {
       $openid->identity = "https://openid.intuit.com/Identity-me";
-      $openid->required = array('contact/email');
-      $openid->optional = array('namePerson', 'namePerson/friendly');
+      $openid->required = array('contact/email', 'namePerson/first', 'namePerson/last');
       header('Location: ' . $openid->authUrl());
     } else {
       dd($openid->getAttributes());
