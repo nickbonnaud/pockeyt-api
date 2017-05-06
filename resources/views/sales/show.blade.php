c877727c5715b3755155071e87b78e059c5adfb1
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-06 15:27:40 -0400

    updating employee roles

--- resources/views/sales/show.blade.php
+++ resources/views/sales/show.blade.php
@@ -10,9 +10,9 @@
 			      Sales Center
 			    </h1>
 			    <h4 style="display: inline-block;" v-show="!customDate">Date Range: Today</h4>
-			    <h4 style="display: inline-block; v-show="customDate">Date Range: </h4>
-			    <a style="display: inline-block; v-show="!customDate" href="#" v-on:click="toggleDate()">Change</a>
-			    <input style="display: inline-block; v-show="customDate" type="text" name="daterange" value="01/01/2015 - 01/31/2015" />
+			    <h4 style="display: inline-block;" v-show="customDate">Date Range: </h4>
+			    <a style="display: inline-block;" v-show="!customDate" href="#" v-on:click="toggleDate()">Change</a>
+			    <input style="display: inline-block;" v-show="customDate" type="text" name="daterange" value="01/01/2015 - 01/31/2015" />
 			    <ol class="breadcrumb">
 			      <li><a href="{{ route('profiles.show', ['profiles' => $user->profile->id])  }}"><i class="fa fa-dashboard"></i> Home</a></li>
 			      <li class="active">Sales Center</li>
