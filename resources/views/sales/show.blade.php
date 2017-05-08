24a34b3bbe338a53823bb6ac31c5fa131847bee9
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-08 10:24:24 -0400

    adding tip tracking

--- resources/views/sales/show.blade.php
+++ resources/views/sales/show.blade.php
@@ -72,8 +72,46 @@
 									</div>
 								</div>
 							</div>
-							<div class="row">
-							</div>
+							@if(!$user->profile->tip_tracking_enabled)
+								<div class="row">
+									<div v-if="employees.length > 0">
+										<div v-for="employee in employees" class="col-md-4">
+											<div class="box box-widget widget-user">
+												<div class="widget-user-header bg-aqua-active">
+													<h3 class="widget-user-username">@{{ employee.first_name }} @{{ employee.last_name }}</h3>
+													<h5 class="widget-user-desc">@{{ employee.role }}</h5>
+												</div>
+												<div class="widget-user-image">
+													<img v-if="employee.photo_path" :src="employee.photo_path" class="img-circle" alt="User Image">
+													<img v-else class="img-circle" src="{{ asset('/images/icon-profile-photo.png') }}">
+												</div>
+												<div class="box-footer">
+													<div class="row">
+														<div class="col-sm-4 border-right">
+															<div class="description-block">
+																<h5 class="description-header">$@{{ employeeSales(employee.id) }}</h5>
+																<span class="description-text">SALES</span>
+															</div>
+														</div>
+														<div class="col-sm-4 border-right">
+															<div class="description-block">
+																<h5 class="description-header">$@{{ employeeTips(employee.id) }}</h5>
+																<span class="description-text">TIPS</span>
+															</div>
+														</div>
+														<div class="col-sm-4">
+															<div class="description-block">
+																<h5 class="description-header">$@{{ employeeTotal(employee.id) }}</h5>
+																<span class="description-text">TOTAL</span>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							@endif
 						</div>
 					</div>
 				</section>
@@ -121,6 +159,7 @@
 
 		data: {
 			transactions: {!! $salesToday !!},
+			employees: {!! $employees !!}
 			fromDate: "today",
 			toDate: "",
 			modalPick: "",
@@ -129,6 +168,42 @@
 		},
 
 		computed: {
+			employeeSales: function(employeeId) {
+				var transactions = this.transactions;
+				if (transactions == 0) { return 0}
+				var total = 0;
+				transactions.forEach(function(transaction) {
+					if (transaction.employee_id == employeeId) {
+						total = total + transaction.net_sales + transaction.tax;
+					}
+				});
+				return (total / 100).toFixed(2);
+			},
+
+			employeeTips: function(employeeId) {
+				var transactions = this.transactions;
+				if (transactions == 0) { return 0}
+				var totalTips = 0;
+				transactions.forEach(function(transaction) {
+					if (transaction.employee_id == employeeId) {
+						totalTips = totalTips + transaction.tips;
+					}
+				});
+				return (totalTips / 100).toFixed(2);
+			},
+
+			employeeTotal: function(employeeId) {
+				var transactions = this.transactions;
+				if (transactions == 0) { return 0}
+				var total = 0;
+				transactions.forEach(function(transaction) {
+					if (transaction.employee_id == employeeId) {
+						total = total + transaction.total;
+					}
+				});
+				return (total / 100).toFixed(2);
+			},
+
 			netSales: function() {
 				var transactions = this.transactions;
 				if (transactions == 0) { return 0}
