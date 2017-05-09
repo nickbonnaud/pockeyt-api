144e5670dc75b4b7444907b3e12776865d87a577
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-09 17:00:12 -0400

    adding employee sign-in page

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -262,7 +262,7 @@
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           <h4 class="modal-title" id="EmployeeChooseModal">Please choose Team Member</h4>
         </div>
-        <div class="modal-body">
+        <div class="modal-body-analytics">
           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="box box-success">
               <div class="box-header with-border">
