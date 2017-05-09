cabd848454f81c53a7993dc5cea3cb17ff70012d
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-09 18:05:56 -0400

    adding employee sign-in page

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -48,10 +48,10 @@ class SalesController extends Controller
     		}
     	}
     }
+    dd($employees);
     if (count($employees == 0)) {
   		$employees = 0;
   	}
-
     return view('sales.show', compact('salesToday', 'employees'));
   }
 
