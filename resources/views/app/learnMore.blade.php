b8ad5a218a93163ac23b5b0e503be304ad1897dc
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-10 12:14:57 -0400

    adding public disconnect view

--- resources/views/app/learnMore.blade.php
+++ resources/views/app/learnMore.blade.php
@@ -1,13 +1,13 @@
 @extends('layout')
 @section('content')
 	<div class="learn-top">
-		<h3>Automatically Sync your Pockeyt transactions with QuickBooks</h3>
-		<p>Keep your QuickBooks account current and up-to-date with Pockeyt Sync</p>
+		<h3>Automatically sync your Pockeyt transactions with QuickBooks.</h3>
+		<p>Keep your QuickBooks account current and up-to-date with Pockeyt Sync.</p>
 	</div>
 	<div class="learn-middle">
 		<img src="{{ asset('/images/sync-image.png') }}">
 	</div>
 	<div class="learn-bottom">
-		<p>To connect your Pockeyt account, please go to the <a href="{{ route('app.index') }}">Pockeyt Dashboard</a> and click Quickbooks in your Inventory tab.</p>
+		<p>To connect to your Pockeyt account, please go to the <a href="{{ route('app.index') }}">Pockeyt Dashboard</a> and click QuickBooks in your Inventory tab.</p>
 	</div>
 @stop
