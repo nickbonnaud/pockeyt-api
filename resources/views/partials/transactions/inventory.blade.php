52774d94cd7701e5ecc69b6e4ad8dd5dbc8eb525
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-09 20:58:50 -0400

    adding employee sign-in page

--- resources/views/partials/transactions/inventory.blade.php
+++ resources/views/partials/transactions/inventory.blade.php
@@ -1,5 +1,5 @@
 <div v-if="inventory.length > 0" >
-	<div class="col-md-3 col-sm-6 col-xs-6" v-for="product in productsFilter">
+	<div class="col-lg-2 col-md-3 col-sm-6 col-xs-12" v-for="product in productsFilter">
     <div v-if="product.product_tn_photo_path" class="box-inventory" v-on:click="addProduct(product)">
       <div class="box-body-inventory-image">
         <img :src="product.product_tn_photo_path">
