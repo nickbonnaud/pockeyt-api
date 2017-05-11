6f52e97fd38d284f7cb9383e7dc82a6d4d7d90b4
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-11 13:37:51 -0400

    updating learn more page

--- resources/views/app/learnMore.blade.php
+++ resources/views/app/learnMore.blade.php
@@ -1,25 +1,32 @@
 @extends('layout')
-@section('content')
-	<div class="learn-top">
-		<h3>Automatically sync your Pockeyt transactions with QuickBooks.</h3>
-	</div>
-	<div class="learn-middle">
-		<div class="col-md-6">
-			<p>Keep your QuickBooks account current and up-to-date with Pockeyt Sync.</p>
-			<img src="{{ asset('/images/sync-image.png') }}">
-			<h4>Features:</h4>
-			<ul>
-				<li>Syncs every transaction on Pockeyt to your QuickBooks Account.</li>
-				<li>Every transaction synced creates an invoice and closes the Invoice once payment confirmed.</li>
-				<li>Pockeyt easily tracks sales, tips, and taxes and automatically syncs them with your QuickBooks account for every transaction.</li>
-				<li>Seamless and automatic once connected!</li>
-			</ul>
+<div class="wrapper">
+  <div class="container" style="background: #ecf0f5; border-radius: 20px; padding: 50px;">
+		<div class="learn-top">
+			<h3>Automatically sync your Pockeyt transactions with QuickBooks.</h3>
 		</div>
-		<div class="col-md-6">
-			<img src="{{ asset('/images/qboConnectScreen.png') }}">
+		<div class="row">
+			<div class="learn-middle">
+				<div class="col-md-6">
+					<p>Keep your QuickBooks account current and up-to-date with Pockeyt Sync.</p>
+					<img src="{{ asset('/images/sync-image.png') }}">
+					<h4>Features:</h4>
+					<ul>
+						<li>Syncs every transaction on Pockeyt to your QuickBooks Account.</li>
+						<li>Every transaction synced creates an invoice and closes the Invoice once payment confirmed.</li>
+						<li>Pockeyt easily tracks sales, tips, and taxes and automatically syncs them with your QuickBooks account for every transaction.</li>
+						<li>Seamless and automatic once connected!</li>
+					</ul>
+				</div>
+				<div class="col-md-6">
+					<img src="{{ asset('/images/qboConnectScreen.png') }}">
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="learn-bottom">
+				<p>To connect to your Pockeyt account, please go to the <a href="{{ route('app.index') }}">Pockeyt Dashboard</a> and click QuickBooks in your Inventory tab.</p>
+			</div>
 		</div>
 	</div>
-	<div class="learn-bottom">
-		<p>To connect to your Pockeyt account, please go to the <a href="{{ route('app.index') }}">Pockeyt Dashboard</a> and click QuickBooks in your Inventory tab.</p>
-	</div>
+</div>
 @stop
