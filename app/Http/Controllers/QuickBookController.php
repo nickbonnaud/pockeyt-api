fbcd6729c106f39283d122a75b66fbb5ebe4fe27
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-16 01:12:04 -0400

    updating qbo

--- app/Http/Controllers/QuickBookController.php
+++ app/Http/Controllers/QuickBookController.php
@@ -115,7 +115,7 @@ class QuickBookController extends Controller
     $customerId = $this->user->profile->account->pockeyt_qb_id;
     $customerService = new \QuickBooks_IPP_Service_Customer();
     if ($customerId) {
-      $qbCustomerId = $customerService->query($this->context, $this->realm, "SELECT * FROM Customer WHERE Id = " . $customerId);
+      $qbCustomerId = $customerService->query($this->context, $this->realm, "SELECT * FROM Customer");
       dd($qbCustomerId);
       if (count($qbCustomerId) != 0) { return; }
     }
