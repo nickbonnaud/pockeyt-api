3b36898234a6727ed0d946a879f78cd8b41d63bd
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-15 22:46:09 -0400

    updating qbo taxes

--- resources/views/qbo/tax.blade.php
+++ resources/views/qbo/tax.blade.php
@@ -5,7 +5,7 @@
 		<div style="text-align: center">
 			<h2>Success! Connected to QuickBooks Online</h2>
 			<h4>Warning</h4>
-			<h4>Your current Sales Tax Rate in Pockeyt is {{ $pockeytTaxRate }}%. Your Sales Tax Rate in QuickBooks is {{ qbTaxRate }}%. In order for Pockeyt to sync with Quickbooks, your Sales Taxes must match.</h4>
+			<h4>Your current Sales Tax Rate in Pockeyt is {{ $pockeytTaxRate }}%. Your Sales Tax Rate in QuickBooks is {{ $qbTaxRate }}%. In order for Pockeyt to sync with Quickbooks, your Sales Taxes must match.</h4>
 			<p>Please correct by adjusting your Sales Tax in QuickBooks or adjusting your location in Pockey*.</p>
 			<p>Once Sales Taxes are matching click the Set Sales Tax Button in you Business Account Profile Section to finish setting up Pockeyt Sync.</p>
 			<button type="button" class="btn btn-block btn-primary btn-lg" onclick="self.close()">Close Window</button>
