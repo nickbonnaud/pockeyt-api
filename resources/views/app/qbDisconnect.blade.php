abc238aaf086b61f275eeaf7356fa0baefeb46d5
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-18 02:05:41 -0400

    updating quickbooks spelling

--- resources/views/app/qbDisconnect.blade.php
+++ resources/views/app/qbDisconnect.blade.php
@@ -1,8 +1,8 @@
 @extends('layout')
 @section('content')
 	<h3>Successfully Disconnected Pockeyt</h3>
-	<p>Pockeyt is no longer connected to your Quickbooks account and will no longer auto-update transactions.</p>
-	<p>To reconnect, please go to the <a href="{{ route('app.index') }}">Pockeyt Dashboard</a> and reconnect Quickbooks in your Inventory tab.</p>
+	<p>Pockeyt is no longer connected to your QuickBooks account and will no longer auto-update transactions.</p>
+	<p>To reconnect, please go to the <a href="{{ route('app.index') }}">Pockeyt Dashboard</a> and reconnect QuickBooks in your Inventory tab.</p>
 	
 @stop
 
