b08ea6fa2c37936b34a950fcd076f897be1509c8
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-17 20:03:09 -0400

    updating push for production

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -134,6 +134,7 @@ class TransactionsController extends Controller
           ->send($message);
 
         foreach ($collection->pushManager as $push) {
+            dd($push->getAdapter()->getResponse());
           $response = $push->getAdapter()->getResponse()->getCode();
         }
 
