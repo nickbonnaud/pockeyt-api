0c6de9835d91c32bd94b8782f373065ba2a8ca52
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-25 00:56:00 -0400

    debugging push

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -152,6 +152,7 @@ class TransactionsController extends Controller
         foreach ($collection->pushManager as $push) {
           $response = $push->getAdapter()->getResponse();
         }
+        dd($response);
         if ($pushService == 'PockeytIOS') {
             if ($response === 0) {
                 $transaction->status = 11;
