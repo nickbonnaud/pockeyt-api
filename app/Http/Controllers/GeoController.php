f27a57d3a988db7406d3612f1db39f5e5a32a1c1
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-05-25 16:22:02 -0400

    updating geofences

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -26,9 +26,9 @@ class GeoController extends Controller
 
     public function getGeoFences() {
         $geoCoords = GeoLocation::with('profile.logo')->get();
-        return response($geoCoords);
         $geoFences = [];
         foreach ($geoCoords as $geoCoord) {
+            return response($geoCoord);
             $data['latitude'] = $geoCoord->latitude;
             $data['longitude'] = $geoCoord->longitude;
             $data['identifier'] = $geoCoord->identifier;
