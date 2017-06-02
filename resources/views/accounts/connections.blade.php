c7b8a7355faf5438d064a053a6f98c900a591549
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-06-01 22:23:39 -0400

    updating pockeyt lite

--- resources/views/accounts/connections.blade.php
+++ resources/views/accounts/connections.blade.php
@@ -28,122 +28,122 @@ $qbo_connect = $qbo_obj->qboConnect();
 								<table class="table no-margin">
 									<thead>
 										<tr>
-											<th>Company</th>
-											<th>Service</th>
-											<th>Change Connection</th>
-											<th>Feature</th>
+											<th class="text-center">Company</th>
+											<th class="text-center">Service</th>
+											<th class="text-center">Connection Status</th>
+											<th class="text-center">Feature Status</th>
 										</tr>
 									</thead>
 									<tbody>
 										<tr>
-											<td><span class="icon-fb-connect"></span></td>
-											<td>Auto Posting</td>
+											<td class="text-center"><span class="icon-fb-connect"></span></td>
+											<td class="text-center">Auto Posting</td>
 											@if($user->profile->fb_app_id !== null)
-												<td><span class="label label-success">Connected</span></td>
+												<td class="text-center"><span class="label label-success">Connected</span></td>
 												@if($user->profile->fb_page_id !== null && $user->profile->connected == 'facebook')
-													<td>
+													<td class="text-center">
 														<a href="{{ action('ConnectController@removefBSubscription') }}">
 															<button class="btn btn-danger">Disable</button>
 														</a>
 													</td>
 												@else
-													<td>
+													<td class="text-center">
 														<a href="{{ action('ConnectController@addfBSubscription') }}">
 															<button class="btn btn-success">Enable</button>
 														</a>
 													</td>
 												@endif
 											@else
-												<td>
+												<td class="text-center">
 													<a href="{{ action('ConnectController@connectFB') }}" class="btn btn-block btn-social btn-facebook">
 											      <i class="fa fa-facebook"></i>
 											      Connect With Facebook
 										  		</a>
 										  	</td>
-												<td><button class="btn btn-success disabled">Enable</button></td>
+												<td class="text-center"><button class="btn btn-success disabled">Enable</button></td>
 											@endif
 										</tr>
 										<tr>
-											<td><span class="icon-insta-connect"></span></td>
-											<td>Auto Posting</td>
+											<td class="text-center"><span class="icon-insta-connect"></span></td>
+											<td class="text-center">Auto Posting</td>
 											@if($user->profile->insta_account_id !== null)
-												<td><span class="label label-success">Connected</span></td>
+												<td class="text-center"><span class="label label-success">Connected</span></td>
 												@if($user->profile->insta_account_token !== null)
-													<td>
+													<td class="text-center">
 														<a href="{{ action('ConnectController@removeInstaSubscription') }}">
 															<button class="btn btn-danger">Disable</button>
 														</a>
 													</td>
 												@else
-													<td>
+													<td class="text-center">
 														<a href="{{ action('ConnectController@connectInsta') }}">
 															<button class="btn btn-success">Enable</button>
 														</a>
 													</td>
 												@endif
 											@else
-												<td>
+												<td class="text-center">
 													<a href="{{ action('ConnectController@connectInsta') }}" class="btn btn-block btn-social btn-instagram">
 	      										<i class="fa fa-instagram"></i>
 	      										Connect With Instagram
   												</a>
   											</td>
-												<td><button class="btn btn-success disabled">Enable</button></td>
+												<td class="text-center"><button class="btn btn-success disabled">Enable</button></td>
 											@endif
 										</tr>
 										<tr>
-											<td><span class="icon-square-connect"></span></td>
-											<td>Inventory Import</td>
+											<td class="text-center"><span class="icon-square-connect"></span></td>
+											<td class="text-center">Inventory Import</td>
 											@if(isset($user->profile->square_token))
-												<td><span class="label label-success">Connected</span></td>
+												<td class="text-center"><span class="label label-success">Connected</span></td>
 												@if($user->profile->account->square_location_id)
-													<td><span class="label label-primary">Enabled while connected</span></td>
+													<td class="text-center"><span class="label label-primary">Enabled while connected</span></td>
 												@else
-													<td>
+													<td class="text-center">
 														<a href="{{ action('ConnectController@getSquareLocationId') }}">
 															<button class="btn btn-success">Enable</button>
 														</a>
 													</td>
 												@endif
 											@else
-												<td>
+												<td class="text-center">
 													<a href="{{ 'https://connect.squareup.com/oauth2/authorize?client_id=' . env('SQUARE_ID') . '&scope=ITEMS_READ%20ITEMS_WRITE%20MERCHANT_PROFILE_READ%20PAYMENTS_READ&state=' . env('SQUARE_STATE') }}" class="btn btn-block btn-social btn-github">
 														<i class="fa fa-square-o"></i>
 														Connect With Square
 													</a>
 												</td>
-												<td><button class="btn btn-success disabled">Enable</button></td>
+												<td class="text-center"><button class="btn btn-success disabled">Enable</button></td>
 											@endif
 										</tr>
 										<tr>
-											<td><span class="icon-square-connect"></span></td>
-											<td>Pockeyt Lite</td>
+											<td class="text-center"><span class="icon-square-connect"></span></td>
+											<td class="text-center">Pockeyt Lite</td>
 											@if(isset($user->profile->square_token))
-												<td><span class="label label-success">Connected</span></td>
+												<td class="text-center"><span class="label label-success">Connected</span></td>
 												@if($user->profile->account->pockeyt_lite_enabled)
-													<td><a href="{{ action('ConnectController@disablePockeytLite') }}"><button class="btn btn-danger">Disable</button></a></td>
+													<td class="text-center"><a href="{{ action('ConnectController@disablePockeytLite') }}"><button class="btn btn-danger">Disable</button></a></td>
 												@else
-													<td><a href="{{ action('ConnectController@subscribeSquare') }}"><button class="btn btn-success">Enable</button></a></td>
+													<td class="text-center"><a href="{{ action('ConnectController@subscribeSquare') }}"><button class="btn btn-success">Enable</button></a></td>
 												@endif
 											@else
-												<td>
+												<td class="text-center">
 													<a href="{{ 'https://connect.squareup.com/oauth2/authorize?client_id=' . env('SQUARE_ID') . '&scope=ITEMS_READ%20ITEMS_WRITE%20MERCHANT_PROFILE_READ%20PAYMENTS_READ&state=' . env('SQUARE_STATE') }}" class="btn btn-block btn-social btn-github">
 														<i class="fa fa-square-o"></i>
 														Connect With Square
 													</a>
 												</td>
-												<td><button class="btn btn-success disabled">Enable</button></td>
+												<td class="text-center"><button class="btn btn-success disabled">Enable</button></td>
 											@endif
 										</tr>
 										<tr>
-											<td><span class="icon-quickbooks-connect"></span></td>
-											<td>Pockeyt Sync</td>
+											<td class="text-center"><span class="icon-quickbooks-connect"></span></td>
+											<td class="text-center">Pockeyt Sync</td>
 											@if($user->profile->connected_qb)
-												<td><button class="btn btn-danger" href="{{ action('QuickBookController@qboDisconnect') }}">Disconnect</button></td>
-												<td><span class="label label-primary">Disable by disconnecting</span></td>
+												<td class="text-center"><button class="btn btn-danger" href="{{ action('QuickBookController@qboDisconnect') }}">Disconnect</button></td>
+												<td class="text-center"><span class="label label-primary">Disable by disconnecting</span></td>
 											@else
-												<td style="text-align: center;"><ipp:connectToIntuit></ipp:connectToIntuit></td>
-												<td><span class="label label-primary">Enable by connecting</span></td>
+												<td class="text-center"><ipp:connectToIntuit></ipp:connectToIntuit></td>
+												<td class="text-center"><span class="label label-primary">Enable by connecting</span></td>
 											@endif
 										</tr>
 									</tbody>
