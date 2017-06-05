1aeeb025e8ca7c538209263b78c06b57957ee985
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-06-05 07:45:53 -0400

    updating square transactions

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -761,10 +761,12 @@ class TransactionsController extends Controller
 
     public function processSquarePayment($payment, $businessAccount, $customerId) {
         $profile = $businessAccount->profile;
-        $customer = User::findOrFail($customerId);
-        $user = $customer;
+
+        $user = $customerId;
         $business = 119;
         event(new CustomerLeaveRadius($user, $business));
+        
+        $customer = User::findOrFail($customerId);
         $transaction = new Transaction;
 
         $transaction->tax = $payment->tax_money->amount;
