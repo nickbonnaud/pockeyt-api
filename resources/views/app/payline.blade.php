7680a4fe7d9a46cb4c69889267adffa4a601ac85
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-06-20 18:05:37 -0400

    updating payments

--- resources/views/app/payline.blade.php
+++ resources/views/app/payline.blade.php
@@ -3,6 +3,7 @@
 
 
 @stop
+<script type="text/javascript" src="https://vgs-assets.s3.amazonaws.com/payline-1.latest.js"></script>
 
 <script type="text/javascript">
   document.addEventListener("DOMContentLoaded", function(event) {
