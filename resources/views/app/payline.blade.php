04112332a3a17d4c6fa8f020446a8c6b97d32ac2
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-06-20 18:57:20 -0400

    updating payments

--- resources/views/app/payline.blade.php
+++ resources/views/app/payline.blade.php
@@ -2,17 +2,7 @@
 <title>Veriforms demo</title>
 </head>
 <body>
-  <button id="show-form">Show form</button>
-  <pre id="preview"></pre>
+	<a href="/mobile/close">Close</a>
 
   <script type="text/javascript" src="https://vgs-assets.s3.amazonaws.com/payline-1.latest.js"></script>
-  <script type="text/javascript">
-    document.addEventListener("DOMContentLoaded", function(event) {
-      document.getElementById('show-form').addEventListener('click', function() {
-        window.close();
-      });
-    });
-  </script>
-
-
 </body>
