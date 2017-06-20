33269431929c3458dfd42d025bdc52d09ff2fe4d
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-06-20 19:30:13 -0400

    debugging payline

--- resources/views/app/payline.blade.php
+++ resources/views/app/payline.blade.php
@@ -2,7 +2,22 @@
 <title>Veriforms demo</title>
 </head>
 <body>
-	<a href="/mobile/close">Close</a>
+  <button id="show-form">Show form</button>
+  <pre id="preview"></pre>
 
   <script type="text/javascript" src="https://vgs-assets.s3.amazonaws.com/payline-1.latest.js"></script>
+  <script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function(event) {
+      document.getElementById('show-form').addEventListener('click', function() {
+        Payline.openTokenizeCardForm({
+          applicationName: 'Pollos Hermanos',
+          applicationId: 'AP3UKRi9QBmgAjv9v4iKuH7T',
+        }, function (tokenizedResponse) {
+          document.getElementById('preview').innerText = JSON.stringify(tokenizedResponse, null, '  ');
+        });
+      });
+    });
+  </script>
+
+
 </body>
