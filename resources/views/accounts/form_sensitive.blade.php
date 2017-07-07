c0dd308af8d5bb74a91f857155e72c160ae808a3
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-07 17:30:08 -0400

    updating styling

--- resources/views/accounts/form_sensitive.blade.php
+++ resources/views/accounts/form_sensitive.blade.php
@@ -14,13 +14,13 @@
 <div class="form-group">
   <label for="accountNumber" class="col-sm-2 control-label">Full Account Number</label>
   <div class="col-sm-10">
-    <input type="text" name="accountNumber" class="form-control" id="accountNumber" required>
+    <input type="text" name="accountNumber" class="form-control" value="XXXXX{{$account->accountNumber}}" id="accountNumber" required>
   </div>
 </div>
 <div class="form-group">
  <label for="routing" class="col-sm-2 control-label">Full Routing Number</label>
   <div class="col-sm-10">
-    <input type="integer" name="routing" class="form-control" id="routing" required>
+    <input type="integer" name="routing" class="form-control" value="XXXXX{{$account->routing}}" id="routing" required>
   </div>
 </div>
 <div class="modal-footer modal-footer-form-tags">
