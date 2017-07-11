0732227dbfdc93ae8f494844f7e57aece1216740
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-11 15:22:50 -0400

    updating masks on edit info

--- resources/views/partials/admin/approve_account.blade.php
+++ resources/views/partials/admin/approve_account.blade.php
@@ -1,5 +1,5 @@
 @if($signedIn && $isAdmin)
-  <form action="{{ route('accounts.approve'), ['accounts' => $account->id]) }}" method="post">
+  <form action="{{ route('accounts.approve', ['accounts' => $account->id]) }}" method="post">
       {{ csrf_field() }}
       <input type="submit" value="Approve" class="btn btn-block btn-success btn-sm">
   </form>
