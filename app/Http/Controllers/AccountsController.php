f1d3383e1566fe5c0eb680836bca8d5e978f383a
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-11 22:51:15 -0400

    updating masks on edit info

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -190,7 +190,7 @@ class AccountsController extends Controller
     }
 
     public function sendToSplash($account, $mcc) {
-        dd(date_format(date_create($account->established), 'YYYYMMDD' ));
+        dd(date_format(date_create($account->established), 'Ymmdd'));
         SplashPayments\Utilities\Config::setSessionKey("nsub8et5IuJ7JP3lvsWrQbK");
         $object = new \SplashPayments\merchants(
             array (
