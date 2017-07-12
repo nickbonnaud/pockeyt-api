51bcdcf477681500819906fd81d2779620c58a2c
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-12 14:23:17 -0400

    updating keys

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -237,6 +237,7 @@ class AccountsController extends Controller
         );
 
         try {
+            dd($object);
             $object->create();
         }
         catch (SplashPayments\Exceptions\Base $e) {
