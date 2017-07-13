d57b251d99f980b9f57dc7d6faa1d608b196941e
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-13 16:23:58 -0400

    updating webhooks

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -239,7 +239,7 @@ class AccountsController extends Controller
         SplashPayments\Utilities\Config::setTestMode(true);
         SplashPayments\Utilities\Config::setApiKey(env('SPLASH_KEY'));
 
-        $object = new SplashPayments\alertActions(
+        $object = new SplashPayments\alertTriggers(
            
         );
         try {
