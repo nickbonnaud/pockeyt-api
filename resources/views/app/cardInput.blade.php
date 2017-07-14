46c4dfa0a92e2929fe78602b26c490482dce8f6b
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-14 15:43:01 -0400

    updating PayFrame

--- resources/views/app/cardInput.blade.php
+++ resources/views/app/cardInput.blade.php
@@ -24,7 +24,6 @@
     PaymentFrame.config.mode = "token";
     PaymentFrame.config.name = "Pockeyt";
     PaymentFrame.config.description = "card vault";
-    PaymentFrame.config.image = "{!! asset('/images/pockeyt-icon-square.png' !!}"
 
     document.addEventListener("DOMContentLoaded", function(event) {
       PaymentFrame.popup();
