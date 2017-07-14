8cfbbc1c92b6571df818b2e16627cac7d34abfbc
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-13 21:54:38 -0400

    updating webhooks

---
+++ database/migrations/2017_07_13_215218_add_splash_id_to_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSplashIdToTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->string('splash_id')->nullable()->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('splashId');
+        });
+    }
+}
