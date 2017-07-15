0c540d5ada8883e41c7f71a4a83f534aceb8efce
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-15 17:12:49 -0400

    updating PayFrame

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -16,6 +16,7 @@ class VerifyCsrfToken extends BaseVerifier {
         'connect/subscribe/facebook',
         'connect/subscribe/instagram',
         'qbo/openid',
-        'square/transaction/receive'
+        'square/transaction/receive',
+        'vault/token'
     ];
 }
