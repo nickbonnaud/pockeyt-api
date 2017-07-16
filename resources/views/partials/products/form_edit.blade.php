e61342c78a7f9e43e3a9aab2ca564c55e31ad63c
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-15 20:30:16 -0400

    updating tracking for transactions

--- resources/views/partials/products/form_edit.blade.php
+++ resources/views/partials/products/form_edit.blade.php
@@ -8,7 +8,7 @@
 
     <div class="form-group">
         <label for="price">Price:</label>
-        <input data-inputmask="'alias': 'numeric', 'groupSeparator': ',', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'prefix': '$ ', 'placeholder': '0'" type="tel" name="price" id="price" class="form-control" required>
+        <input data-inputmask="'alias': 'numeric', 'groupSeparator': ',', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'prefix': '$ ', 'placeholder': '0'" type="tel" name="price" id="price" value="{{ $product->price }}" class="form-control" required>
     </div>
 
     <div class="form-group">
