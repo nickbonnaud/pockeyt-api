be67ab4be9541df8fefd29a53be32046ab433a2e
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-16 17:01:42 -0400

    updating category

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -50,6 +50,7 @@ class ProductsController extends Controller {
 
   public function edit (Request $request, $id) {
   	$product = Product::findOrFail($id);
+    $profile = $this->user->profile;
     $categories = Product::where('profile_id', '=', $profile->id)->pluck('category');
     dd($categories);
     $product->price = $product->price / 100;
