93daf6046c3bcba952dc1360f3aeb5071431f99a
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-21 10:09:55 -0400

    updating txn

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -844,7 +844,7 @@ class TransactionsController extends Controller
         $transactions = Transaction::where([
             ['profile_id', '=', $profile->id],
             ['paid', '=', true]
-        ])->orderBy('updated_at', 'desc')->take(10)->leftJoin('transactions', 'users.id', '=', 'transactions.user_id')->get();
+        ])->leftJoin('transactions', 'users.id', '=', 'transactions.user_id')->orderBy('updated_at', 'desc')->take(10)->get();
         dd($transactions);
         return view('transactions.refund', compact('transactions'));
     }
