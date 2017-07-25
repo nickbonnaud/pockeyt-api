088be4221eb8c16b9b077ada7d37f773645d254a
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-07-25 18:35:56 -0400

    updating refund page

--- resources/views/transactions/refund.blade.php
+++ resources/views/transactions/refund.blade.php
@@ -22,11 +22,12 @@
 							<li><a href="#" v-on:click="setSelection('ID')">Receipt ID</a></li>
 						</ul>
 					</div>
-					<input v-model="searchInput" name="searchInput" type="text" class="form-control" :disabled="searchSelection == 'Search By'">
+					<input v-model="searchInput" type="text" class="form-control" :disabled="searchSelection == 'Search By'">
 					<span class="input-group-btn">
 						<button type="button" class="btn btn-success btn-flat" :disabled="searchInput == ''">Search</button>
 					</span>
 				</div>
+				<p>@{{ searchInput }}</p>
 				<div class="col-md-6">
 				
 					
