527ded9d5f41bc9d8f961bc9a62f63f03760c697
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-01 12:33:57 -0400

    add refund id to transactions

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -890,6 +890,7 @@ class TransactionsController extends Controller
     }
 
     public function refundSubmitPartial(Request $request) {
+        dd($request->all());
         $transaction = Transaction::findOrFail($request->id);
 
         $refundAmount = $request->total_new;
