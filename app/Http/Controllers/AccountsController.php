f54448ca2602cb151ac123e7b664f5081206f16c
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-01 19:33:46 -0400

    masking route model bindings

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -129,7 +129,7 @@ class AccountsController extends Controller
         $account->status = 'review';
         $account->save();
         $account = $this->shortenSensitive($account);
-        return view('accounts.edit', compact('account'));
+        return redirect()->route('accounts.edit', ['accounts' => Crypt::encrypt($account->id)]);
     }
 
     public function changePay(UpdateAccountPayRequest $request, $id)
@@ -145,7 +145,7 @@ class AccountsController extends Controller
         $account->status = 'review';
         $account->save();
         $account = $this->shortenSensitive($account);
-        return view('accounts.edit', compact('account'));
+        return redirect()->route('accounts.edit', ['accounts' => Crypt::encrypt($account->id)]);
     }
 
 
