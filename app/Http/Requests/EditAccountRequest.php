708517e79df23e281e0762ad6f89d1265e313e45
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-01 17:10:55 -0400

    add refund id to transactions

--- app/Http/Requests/EditAccountRequest.php
+++ app/Http/Requests/EditAccountRequest.php
@@ -11,7 +11,7 @@ class EditAccountRequest extends Request {
     public function authorize() {
     	$account = $this->route()->parameter('accounts');
     	$user = \Auth::user();
-    	dd($account);
+    	dd($user->profile->account);
     	return $user->profile()->id == $account->profile_id;
     }
 
