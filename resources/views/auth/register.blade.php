f24c4ab0cf5bf0e48e40e6496fdffdbcf8dcb021
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-02 14:38:59 -0400

    masking route model bindings

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,19 +28,22 @@
 
                 <div class="form-group">
                     <label for="password">Password:</label>
-                        <input v-validate data-vv-rules="{ 
+                        <input class="form-control" v-validate="{
+                            rules: 
+                                { 
                                     regex: /^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\X])(?=.*[!$#%=]).*$/,
                                     required: true,
                                     confirmed: 'password_confirmation',
                                     min: 9,
                                     max: 72, 
-                                }"
-                            name="password" type="password" class="form-control">
+                                }
+                            }"
+                            name="password" type="password">
                 </div>
 
                 <div class="form-group">
                     <label for="password_confirmation">Confirm Password:</label>
-                    <input name="password_confirmation" type="password" class="form-control">
+                    <input class="form-control" name="password_confirmation" type="password">
                 </div>
                 <span v-show="errors.has('password')" class="help is-danger">@{{ errors.first('password') }}</span>
 
