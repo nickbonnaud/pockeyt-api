1481f5e649621c4e87f2f117edad8dbd5bc49526
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-02 12:04:50 -0400

    masking route model bindings

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,9 +26,9 @@
                            required>
                 </div>
 
-                <div class="form-group">
+                
                     <label for="password">Password:</label>
-                    <p :class="{ 'form-control': true }"></p>
+                    <p :class="{ 'control': true }"></p>
                         <input v-validate="{
                             rules: 
                                 { 
@@ -42,7 +42,7 @@
                             :class="{'input': true, 'is-danger': errors.has('password') }" name="password" type="password" required>
                         
                         <span v-show="errors.has('password')" class="help is-danger">@{{ errors.first('password') }}</span>
-                </div>
+               
 
                 <div class="form-group">
                     <label for="password_confirmation">Confirm Password:</label>
