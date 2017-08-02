c32491a609a2e83a5d3614b491c4458c850c16ca
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-01 20:01:43 -0400

    masking route model bindings

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -85,7 +85,7 @@ class ProductsController extends Controller {
       foreach ($products as $product) {
         $product->price = ($product->price) / 100;
       }
-  	return redirect()->route('products.list', ['profiles' => Crypt::encrypt($user->profile->id)]);
+  	return redirect()->route('products.list', ['profiles' => Crypt::encrypt($profile->id)]);
   }
 
   public function destroy(DeleteProductRequest $request, $id) {
