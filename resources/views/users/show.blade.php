077401baadd83c35c609e8447780a35f9593b790
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-02 21:34:00 -0400

    masking route model bindings

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -137,7 +137,6 @@
   </div>
 </div>
 <!-- /.content-wrapper -->
-@stop
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.0.1/vue.js"></script>
 <script src="{{ asset('/vendor/veeValidate/vee-validate.js') }}"></script>
 <script>
@@ -156,7 +155,7 @@
       el: '#main'
   });
 </script>
-
+@stop
 @section('scripts.footer')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/dropzone.js"></script>
 
