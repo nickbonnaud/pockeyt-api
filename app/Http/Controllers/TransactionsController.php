c7cfcc167f2f4a42b901ea2ee3090721268fa896
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-10 17:12:31 -0400

    creating email refund receipt

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -386,10 +386,7 @@ class TransactionsController extends Controller
             $result->create();
         }
         catch (SplashPayments\Exceptions\Base $e) {
-            $msg = $e;
-            $code = "Fail to Send to Splash Charge";
-            $this->sendEmailError($profile, $customer, $transaction, $msg, $code);
-            return $success = false;
+            
         }
         if ($result->hasErrors()) {
             $err = $result->getErrors();
