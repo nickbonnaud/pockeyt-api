a9c4d436d060034bdfdc56d1907073b6b06aad3a
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-09 23:59:05 -0400

    fixing form model binding

--- resources/views/partials/loyalty-programs/form_create.blade.php
+++ resources/views/partials/loyalty-programs/form_create.blade.php
@@ -13,14 +13,14 @@
 			</label>
 		</div>
 	</div>
-	<div class="form-group" v-if= "selection == 'increments'">
+	<div class="form-group" v-show= "selection == 'increments'">
 		<label for="purchases_required">Number of Purchases required for reward</label>
 		<div class="input-group col-xs-5 col-md-3">
 			<span class="input-group-addon">#</span>
 			<input class="form-control" type="number" min="1" step="1" name="purchases_required" id="purchases_required" placeholder="20">
 		</div>
 	</div>
-	<div class="form-group" v-if= "selection == 'amounts'">
+	<div class="form-group" v-show= "selection == 'amounts'">
 		<label for="amount_required">Total amount customers must spend to receive reward</label>
 		<div class="input-group col-xs-5 col-md-3">
 			<span class="input-group-addon">$</span>
