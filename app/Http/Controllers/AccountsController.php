a1952f42a8c9b68bdee3e3915a4fd393a22b8a50
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-09 23:22:47 -0400

    fixing form model binding

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -122,7 +122,7 @@ class AccountsController extends Controller
 
     public function changeBusiness(UpdateAccountBusinessRequest $request, $id)
     {
-        dd(round(preg_replace("/[^0-9\.]/","",$request->annualCCSales)));
+        dd(round(preg_replace("/[^0-9\.]/","",$request->annualCCSales), 0));
 
         $account = Account::findOrFail($id);
         $account->update($request->except('annualCCSales'));
