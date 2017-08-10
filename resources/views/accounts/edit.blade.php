0e3cdb9d6e85e9cccfea30f6c9b4df8f170b6e4f
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-09 20:53:25 -0400

    fixing form model binding

--- resources/views/accounts/edit.blade.php
+++ resources/views/accounts/edit.blade.php
@@ -262,7 +262,7 @@ $qbo_connect = $qbo_obj->qboConnect();
   });
 
   	Vue.use(VueMask.VueMaskPlugin);
-  	Vue.use(window['VMoney']);
+  	Vue.use(VMoney);
     var account = new Vue({
       el: '#account',
 
