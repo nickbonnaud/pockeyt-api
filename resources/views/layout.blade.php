8c9bc21ac96d93dcd64de12e726103ffeb3f16c2
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-11 19:25:12 -0400

    updating geo locations for approved only

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="apple-mobile-web-app-title" content="Pockeyt Business">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <title>Pockeyt Business</title>
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/libs.css') }}">
