7c9b487450fba048cbb645a05a1edd313e52f738
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-14 19:39:17 -0400

    updating geo locations for approved only

--- public/vendor/standAlone/stand-alone.js
+++ public/vendor/standAlone/stand-alone.js
@@ -1,1 +1,20 @@
-(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"//"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
+(function(document,navigator,standalone) {
+  // prevents links from apps from oppening in mobile safari
+  // this javascript must be the first script in your <head>
+  if ((standalone in navigator) && navigator[standalone]) {
+    var curnode, location=document.location, stop=/^(a|html)$/i;
+    document.addEventListener('click', function(e) {
+      curnode=e.target;
+      while (!(stop).test(curnode.nodeName)) {
+        curnode=curnode.parentNode;
+      }
+      // Condidions to do this only on links to your own app
+      // if you want all links, use if('href' in curnode) instead.
+      if(
+        'href' in curnode) {
+        e.preventDefault();
+        location.href = curnode.href;
+      }
+    },false);
+  }
+})(document,window.navigator,'standalone');
