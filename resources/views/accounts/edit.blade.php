6b5d8ed9c6d189ded17bb7f1c908a79c2f124818
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-08-14 17:40:28 -0400

    updating geo locations for approved only

--- resources/views/accounts/edit.blade.php
+++ resources/views/accounts/edit.blade.php
@@ -12,7 +12,7 @@ $qbo_connect = $qbo_obj->qboConnect();
 		      Your Business Account Profile
 		    </h1>
 		     @if(!$qbo_connect)
-		    	<span class="pull-right" style="margin-top: -5px;">
+		    	<span class="pull-right quick-button" style="margin-top: -5px;">
 						<ipp:connectToIntuit></ipp:connectToIntuit>
 					</span>
 				@else
